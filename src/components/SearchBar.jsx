9593dbb796d373ac58d5e954555885cc6ec2914d
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-09-04 20:06:30 -0300

    Teste - Iniciando o projeto

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,28 +1,7 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  // noooo comitei na master
-  render() {
-    const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
-    } = this.props;
-
-    return (
-      <div>
-        <span>{ searchText }</span>
-        <span>{ onSearchTextChange } </span>
-        <span>{ bookmarkedOnly }</span>
-        <span>{ onBookmarkedChange }</span>
-        <span>{ selectedGenre }</span>
-        <span>{ onSelectedGenreChange }</span>
-      </div>
-    );
-  }
+  // Desfazendo o requisito 1 do Rodrigo mergeado por engano.
 }
 
-export default SearchBar
+export default SearchBar;
