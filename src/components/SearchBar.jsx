af028f42c32c182644d809bf5d33c930394ba495
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-09-04 14:24:42 -0300

    Requisito 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,35 @@
 // implement SearchBar component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    // const {
+    //   filte: {
+    //     searchText,
+    //     onSearchTextChange,
+    //     bookmarkedOnly,
+    //     onBookmarkedChange,
+    //     selectedGenre,
+    //     onSelectedGenreChange,
+    //   },
+    // } = this.props;
+
+    return (
+      <input id="search" />
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  filte: PropTypes.shape({
+    searchText: PropTypes.string,
+    onSearchTextChange: PropTypes.func,
+    bookmarkedOnly: PropTypes.bool,
+    onBookmarkedChange: PropTypes.func,
+    selectedGenre: PropTypes.string,
+    onSelectedGenreChange: PropTypes.func,
+  }).isRequired,
+};
+
+export default SearchBar;
