1ecf6626b62bacce5883e46397f25ac729c92ede
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-09-04 17:48:54 -0300

    impl: requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,30 +1,42 @@
 // implement AddMovie component here
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 class AddMovie extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      subtitle: '',
+      // subtitle: '',
       title: '',
-      imagePath: '',
-      storyline: '',
-      rating: 0,
-      genre: 'action',
+      // imagePath: '',
+      // storyline: '',
+      // rating: 0,
+      // genre: 'action',
     };
   }
 
   render() {
-    const { onClick } = this.props;
+    const { title } = this.state;
+    // const { onClick } = this.props;
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title-input">
+          Título
+          <input
+            data-testid="title-input"
+            type="text"
+            name="title-input"
+            value={ title }
+            onChange={ ({ target: { value } }) => { this.setState({ title: value }); } }
+          />
+        </label>
+      </form>
     );
   }
 }
 
-AddMovie.propTypes = {
-  onclick: PropTypes.func.isRequired,
-};
+// AddMovie.propTypes = {
+//   onclick: PropTypes.func.isRequired,
+// };
 
 export default AddMovie;
