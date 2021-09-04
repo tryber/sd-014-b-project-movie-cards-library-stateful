a6fa44173576d75c13d9c02d8ea7a6a2f45e190b
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-09-04 19:01:44 -0300

    impl: requisito 12

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,6 +1,7 @@
 // implement AddMovie component here
 import React, { Component } from 'react';
 import ImagePath from './ImagePath';
+import Rating from './Rating';
 import Storyline from './Storyline';
 import Subtitle from './Subtitle';
 import Title from './Title';
@@ -14,13 +15,13 @@ class AddMovie extends Component {
       title: '',
       imagePath: '',
       storyline: '',
-      // rating: 0,
+      rating: 0,
       // genre: 'action',
     };
   }
 
   render() {
-    const { title, subtitle, imagePath, storyline } = this.state;
+    const { title, subtitle, imagePath, storyline, rating } = this.state;
     // const { onClick } = this.props;
     return (
       <form data-testid="add-movie-form">
@@ -46,6 +47,12 @@ class AddMovie extends Component {
           storyline={ storyline }
           onChange={
             ({ target: { value } }) => { this.setState({ storyline: value }); }
+          }
+        />
+        <Rating
+          rating={ rating }
+          onChange={
+            ({ target: { value } }) => { this.setState({ rating: Number(value) }); }
           }
         />
       </form>
