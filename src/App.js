503874eff93615f79fdacdef39db991df44a4797
Luiz Gustavo <gusstavopdm@gmail.com>
2021-09-04 11:47:52 -0300

    Requisito - 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Header from './components/Header';
 import './App.css';
+import Header from './components/Header';
+import movies from './data';
+import MovieList from './components/MovieList';
+// import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      {/* <SearchBar /> */}
+      <MovieList movies={ movies } />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,58 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    // const {
+    //   searchText,
+    //   onSearchTextChange,
+    //   bookmarkedOnly,
+    //   onBookmarkedChange,
+    //   selectedGenre,
+    //   onSelectedGenreChange } = this.props;
+
+    // searchText, uma string
+    // onSearchTextChange, uma callback
+    // bookmarkedOnly, um boolean
+    // onBookmarkedChange, uma callback
+    // selectedGenre, uma string
+    // onSelectedGenreChange, uma callback
+
+    return (
+      <div className={ 0 }>
+        <label htmlFor="searchBar">
+          {text}
+          <input
+            className={ 0 }
+            name={ 0 }
+            id="searchBar"
+            type={ 0 }
+            value={ 0 }
+            onChange={ 0 }
+            required
+          />
+        </label>
+      </div>
+    );
+  }
+}
+
+// SearchBar.propTypes = {
+//   movie: PropTypes.shape({
+//     searchText: PropTypes.string,
+//     onSearchTextChange: PropTypes.string,
+//     bookmarkedOnly: PropTypes.string,
+//     onBookmarkedChange: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+//     selectedGenre: PropTypes.string,
+//     onSelectedGenreChange: PropTypes.string,
+//   }).isRequired,
+// };
+
+// searchText, uma string
+// onSearchTextChange, uma callback
+// bookmarkedOnly, um boolean
+// onBookmarkedChange, uma callback
+// selectedGenre, uma string
+// onSelectedGenreChange, uma callback
+
+export default SearchBar;
