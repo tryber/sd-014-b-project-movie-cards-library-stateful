98adfb2c573807b09cda087d6ed7ec0972fd36c0
Rodrigo Cavalheiro <oldcavalheiro@gmail.com>
2021-09-04 17:06:34 -0300

    [Rodrigo Cavalheiro]project-movie-cards-library-stateful

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,27 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
+    return (
+      <div>
+        <span>{ searchText }</span>
+        <span>{ onSearchTextChange } </span>
+        <span>{ bookmarkedOnly }</span>
+        <span>{ onBookmarkedChange }</span>
+        <span>{ selectedGenre }</span>
+        <span>{ onSelectedGenreChange }</span>
+      </div>
+    );
+  }
+}
+
+export default SearchBar
