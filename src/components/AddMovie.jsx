dedfe99bd75b3e27692b43a6183563303b5fd1af
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-09-05 00:57:41 -0300

    Requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,40 @@
 // implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  handleInputChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const { addMovie } = this.props;
+    return (
+      <form>
+        <input />
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  addMovie: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
