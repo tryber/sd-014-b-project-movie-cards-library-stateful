5a6ee36ef7a5e7bbc43537b76bc520dd48c22f35
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-09-05 16:55:49 -0300

    Atende requisito 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class SearchBar extends React.Component {
   render() {
     const { searchText, onSearchTextChange, bookmarkedOnly,
-    onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
         <label htmlFor="s" data-testid="text-input-label">
@@ -14,7 +14,15 @@ class SearchBar extends React.Component {
           Mostrar somente favoritos
           <input id="c" type="checkbox" data-testid="checkbox-input" onChange={ onBookmarkedChange } checked={ bookmarkedOnly } />
         </label>
-
+        <label htmlFor="se" data-testid="select-input-label">
+          Filtrar por gênero
+          <select id="se" data-testid="select-input" value={ selectedGenre } onChange={ onSelectedGenreChange }>
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
 
       </form>
     );
