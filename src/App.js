b141b353f876e517a1f2e343fecff80912440b01
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-09-05 18:56:52 -0300

    Atende requisito 8

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Header from './components/Header';
 import './App.css';
 import SearchBar from './components/SearchBar';
+import AddMovie from './components/AddMovie';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <SearchBar />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <SearchBar />
+        <AddMovie />
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,36 +12,84 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange({ target }) {
+    if (target.id === 'title') {
+      this.setState({
+        [target.id]: target.value,
+      });
+    }
+    if (target.id === 'subtitle') {
+      this.setState({
+        [target.id]: target.value,
+      });
+    }
+    if (target.id === 'image') {
+      this.setState({
+        [target.id]: target.value,
+      });
+    }
+    if (target.id === 'story') {
+      this.setState({
+        [target.id]: target.value,
+      });
+    }
+    if (target.id === 'rating') {
+      this.setState({
+        [target.id]: target.value,
+      });
+    }
+    if (target.id === 'genre') {
+      this.setState({
+        [target.id]: target.value,
+      });
+    }
+  }
+
+  //   handleChange({ target }) {
+  //     if (target.id === 'title') {
+  //       this.setState({
+  //         [target.id]: target.value,
+  //       });
+  //     }
+  //   }
 
   render() {
     const { onClick } = this.props;
     const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label htmlFor="title">
+        <label htmlFor="title" data-testid="title-input-label">
           Título
-          <input type="text" id="title" value={ title } />
+          <input
+            type="text"
+            id="title"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.handleChange }
+          />
         </label>
         <label htmlFor="subtitle">
           Subtítulo
-          <input type="text" id="subtitle" value={ subtitle } />
+          <input type="text" id="subtitle" value={ subtitle } onClick={ onClick } />
         </label>
         <label htmlFor="image">
           Imagem
-          <input type="text" id="image" value={ imagePath } />
+          <input type="text" id="image" value={ imagePath } onClick={ onClick } />
         </label>
         <label htmlFor="story">
           Sinpse
-          <input type="text" id="story" value={ storyline } />
+          <input type="text" id="story" value={ storyline } onClick={ onClick } />
         </label>
         <label htmlFor="rating">
           Avaliação
-          <input type="number" id="rating" value={ rating } />
+          <input type="number" id="rating" value={ rating } onClick={ onClick } />
         </label>
         <label htmlFor="genre">
           Gênero
-          <input type="text" id="genre" value={ genre } />
+          <input type="text" id="genre" value={ genre } onClick={ onClick } />
         </label>
 
       </form>
