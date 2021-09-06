ac4da864e00799fbdde15aa84532c87d10f4a00f
Vinicius Pereira <pereiravinao@MacBook-Pro-de-Vinicius.local>
2021-09-06 16:25:11 -0300

    Requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 
 export default class AddMovie extends React.Component {
   render() {
-    const { title } = this.state;
+    const { title, subtitle } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label ata-testid="title-input-label">
           Título
-          <input data-testid="title-input" type="text" value={ title } onChange={() => {}}/>
-          </label>
+          <input data-testid="title-input" type="text"
+          value={ title } onChange={() => {}}/>
+        </label>
+        <label data-testid="subtitle-input-label" htmlFor="">
+          Subtítulo
+          <input type="text" name="" data-testid="subtitle-input"
+            value={ subtitle } onChange={() => {}}/>
+        </label>
       </form>
     )
   }
