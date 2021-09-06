c97ec1fdc514709d04faa6bbe771f5f9561ad7b2
Thyara <thyaranonato@outlook.com>
2021-09-06 13:47:16 -0300

    Adiciona requisito 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -13,6 +13,7 @@ class SearchBar extends React.Component {
         <p>{onBookmarkedChange}</p>
         <p>{selectedGenre}</p>
         <p>{onSelectedGenreChange}</p>
+        <form data-testid="search-bar-form" />
       </div>
     );
   }
