5e304058e1fd62d404dd4dcc74cc3159cad5a776
Juliana Braga <juliana4braga@gmail.com>
2021-09-06 16:27:35 -0300

    Construção SearchBar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 import Header from './components/Header';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.state = {
+    };
+  }
+
+  onSearchTextChange() {
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
