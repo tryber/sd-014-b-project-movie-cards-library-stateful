5812475435a637a241dc5abb630e1e4473f39656
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-09-06 15:33:09 -0400

    Requisito 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -14,13 +14,23 @@ class SearchBar extends React.Component {
         <label data-testid="text-input-label" htmlFor="text-input">
           Inclui o texto:
           <input
+            type="text"
             name="text-input"
             data-testid="text-input"
             onChange={ onSearchTextChange }
             value={ searchText }
           />
         </label>
-        {bookmarkedOnly}
+        <label htmlFor="checked-input" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            name="checked-input"
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+        </label>
         {onBookmarkedChange}
         {selectedGenre}
         {onSelectedGenreChange}
