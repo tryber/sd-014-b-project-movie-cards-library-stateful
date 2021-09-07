b3af864d1ec36e1ca9b5e242463cf6fa246e2bf0
LucasAccurcio <lucas.accurcio@gmail.com>
2021-09-07 00:07:58 -0300

    Inicio requisito 01

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,2 +1,24 @@
-// implement SearchBar component here
-// implemente o componente aqui
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Inclui o texto:</p>
+        <input type="text" placeholder="Inclui o texto" />
+        <br />
+        <label htmlFor="show-only-favorites">
+          <input type="checkbox" name="show-only-favorites" id="show-only-favorites" />
+          Mostrar somente favoritos
+        </label>
+        <p>Filtrar por gênero</p>
+        <select>
+          <option value="">Selecione um filtro</option>
+          <option value="action">Ação</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
