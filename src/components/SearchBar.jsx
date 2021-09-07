cd2478a9fa4e45f5bbf663e938f1788be2e5acd1
Cassio Mascarenhas <csmlvx@gmail.com>
2021-09-07 07:53:13 -0300

    SearchBar - Requisito 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -24,6 +24,16 @@ class SearchBar extends Component {
               onChange={ onSearchTextChange }
             />
           </label>
+
+          <label htmlFor="checkbox-input" data-testid="checkbox-input-label"> 
+          Mostrar somente favoritos
+            <input
+               type="checkbox"
+               data-testid="checkbox-input"
+               checked={ bookmarkedOnly }
+               onChange={ onBookmarkedChange }
+            />
+          </label>
         </form>
       </div>
     );
