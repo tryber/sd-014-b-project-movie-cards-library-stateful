16adadae0132b20af5007a55ae6c68d80c2b5d68
Cassio Mascarenhas <csmlvx@gmail.com>
2021-09-07 08:04:18 -0300

    SearchBar -  Requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -24,16 +24,29 @@ class SearchBar extends Component {
               onChange={ onSearchTextChange }
             />
           </label>
-
-          <label htmlFor="checkbox-input" data-testid="checkbox-input-label"> 
-          Mostrar somente favoritos
+          <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
+            Mostrar somente favoritos
             <input
-               type="checkbox"
-               data-testid="checkbox-input"
-               checked={ bookmarkedOnly }
-               onChange={ onBookmarkedChange }
+              type="checkbox"
+              data-testid="checkbox-input"
+              checked={ bookmarkedOnly }
+              onChange={ onBookmarkedChange }
             />
           </label>
+          <label htmlFor="select-input" data-testid="select-input-label">
+            Filtrar por gênero
+            <select
+              data-testid="select-input"
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+            >
+              <option data-testid="select-option" value="">Todos</option>
+              <option data-testid="select-option" value="action">Ação</option>
+              <option data-testid="select-option" value="comedy">Comédia</option>
+              <option data-testid="select-option" value="thriller">Suspense</option>
+            </select>
+          </label>
+
         </form>
       </div>
     );
