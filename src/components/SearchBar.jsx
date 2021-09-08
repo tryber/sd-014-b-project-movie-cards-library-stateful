53b7e241ec33d972535e2252fba305ccca0030a3
icaroemanuel <icaro.emanuel@outlook.com>
2021-09-08 16:05:49 -0300

    Realizando correções do lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -37,7 +37,12 @@ class SearchBar extends React.Component {
           </label>
           <label htmlFor="select-search-input" data-testid="select-input-label">
             Filtrar por gênero
-            <select name="" id="" value={ selectedGenre } onChange={ onSelectedGenreChange } data-testid="select-input" id="select-search-input">
+            <select
+              id="select-search-input"
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+              data-testid="select-input"
+            >
               <option value="" data-testid="select-option">Todos</option>
               <option value="action" data-testid="select-option">Ação</option>
               <option value="comedy" data-testid="select-option">Comédia</option>
