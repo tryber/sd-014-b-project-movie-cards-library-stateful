7ec16ad82bcddd856017bdff213c3b3e7a6c496a
Davisralves <davialves324@gmail.com>
2021-09-08 14:58:10 -0300

    finalizando requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -9,13 +9,30 @@ class AddMovie extends React.Component {
       imagePath: '',
       storyline: '',
       rating: 0,
-      genre: 'action'
+      genre: 'action',
     };
   }
 
+  handleClick = (event) => {
+    const { value, name } = event.target;
+    this.setState({ [name]: value });
+  }
+
   render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label htmlFor="title" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            name="title"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.handleClick }
+          />
+        </label>
+      </form>
     );
   }
 }
