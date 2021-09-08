6999cbdae7a314b9a80cd1cbb79a5a508f6e7b00
Cecilia Cacais <ceci.cacais@gmail.com>
2021-09-08 18:21:08 -0300

    Input do titulo em AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,25 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
-    constructor() {
-      super()
+  constructor() {
+    super();
 
-      this.state = {
-        subtitle: '',
-        title: '',
-        imagePath: '',
-        storyline: '',
-        rating: 0,
-        genre: ''
-      };
+    this.onClick = this.onClick.bind(this);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: ''
     };
-    render() {
-        return (
+  }
 
-        );
-    }
+  onClick(event) {
+    console.log(event.target.value);
+  }
+
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="tituloNovo" data-testid="title-input-label">
+          Título
+          <input
+            id="tituloNovo"
+            type="text"
+            data-testid="title-input"
+            value={ this.state.title }
+            onChange={ this.onClick }
+          />
+        </label>
+      </form>
+    );
+  }
 }
 
 AddMovie.propTypes = {
-    onClick: PropTypes.func.isRequired,
-}
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
