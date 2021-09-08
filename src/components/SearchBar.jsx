606824d5b616796b890a626cf3aa6dcfe0f9fe4b
Kauan Carvalho <kauancarvalho987@gmail.com>
2021-09-08 15:28:05 -0300

    requisito 04

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,9 +5,13 @@ class SearchBar extends Component {
   render() {
     return (
       <form data-testid="search-bar-form">
-        <label htmlFor="" data-testid="text-input-label">
+        <label htmlFor="inputText" data-testid="text-input-label">
           Inclui o texto:
-          <input type="text" data-testid="text-input" onChange={ this.props.onSearchTextChange } value={ this.props.searchText } />
+          <input id="inputText" type="text" data-testid="text-input" onChange={ this.props.onSearchTextChange } value={ this.props.searchText } />
+        </label>
+        <label htmlFor="inputCheckbox" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input data-testid="checkbox-input" type="checkbox" id="inputCheckbox" onChange={ this.props.onBookmarkedChange } checked={ this.props.bookmarkedOnly } />
         </label>
       </form>
     );
