87fdeb1b4409338d531bd57ff59883ce5d6ebe21
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-09-08 17:00:09 -0300

    Requisito 3 Implementado PropType

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
@@ -12,7 +13,7 @@ class SearchBar extends React.Component {
     } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label htmlFor="Seach" data-testid="text-input-label" >
+        <label htmlFor="SeachMovie" data-testid="text-input-label" >
           Inclui o texto:
           <input
             value={ searchText }
@@ -24,5 +25,14 @@ class SearchBar extends React.Component {
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
