6ef14f2f1e9eb33b6c1cb408b83899d454a21dff
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2021-09-08 18:29:08 -0300

    Requisitos 1 ao 5

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,29 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+// import MovieList from './MovieList';
+// import SearchBar from './SearchBar';
+// import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section>
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </section>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })).isRequired,
+};
+
+export default MovieLibrary;
