a1d38b749f6511e242d1d76d718f4748d7ae94cd
Thiago Frozzi <thiagofrozzi@hotmail.com>
2021-09-08 15:36:30 -0300

    Requisitos ate o 6 feitos

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,39 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    }
+    
+    this.handleChange = this.handleChange.bind(this);
+
+    handleChange(event) {
+      this.setState({
+        title: event.target.value,
+      });
+    }
+  }
+  render() {
+    const { onClick } = this.props;
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label">
+        Título:
+          <input data-testid="title-input" onChange={this.handleChange} type="text" value={this.state.title} />
+        </label>
+
+      </form>
+    )
+  }
+}
+
+export default AddMovie;
