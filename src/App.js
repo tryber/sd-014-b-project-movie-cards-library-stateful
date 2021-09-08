d0fd7e8721a9dbc3212166766bb28f03f99c4099
Gustavo <gus.inacio@hotmail.com>
2021-09-08 19:41:45 -0300

    Requirements from #6 to #10

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import AddMovie from './components/AddMovie';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
+      <AddMovie />
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,66 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      /* storyline: '',
+      rating: 0,
+      genre: 'action', */
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const { title, subtitle, imagePath } = this.state;
+    return (
+      <div>
+        <form data-testid="add-movie-form">
+          <label data-testid="title-input-label" htmlFor="title-input">
+            Título
+            <input
+              data-testid="title-input"
+              name="title"
+              type="text"
+              id="title-input"
+              value={ title }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+            Subtítulo
+            <input
+              name="subtitle"
+              type="text"
+              data-testid="subtitle-input"
+              value={ subtitle }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="image-input" data-testid="image-input-label">
+            Imagem
+            <input
+              type="text"
+              name="imagePath"
+              data-testid="image-input"
+              value={ imagePath }
+              onChange={ this.handleChange }
+            />
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
