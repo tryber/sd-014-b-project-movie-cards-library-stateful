1c1207193e0cceaf2209a6728b95966670ee570b
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2021-09-08 03:51:04 -0300

    Requisito 5 OK

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,8 +8,6 @@ class SearchBar extends Component {
       onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        { searchText + onSearchTextChange + bookmarkedOnly
-        + onBookmarkedChange + selectedGenre + onSelectedGenreChange }
         <label htmlFor="search-text" data-testid="text-input-label">
           Inclui o texto:
           <input
@@ -29,6 +27,20 @@ class SearchBar extends Component {
             checked={ bookmarkedOnly }
             onChange={ onBookmarkedChange }
           />
+        </label>
+        <label htmlFor="movie-genre" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            id="movie-genre"
+            data-testid="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
         </label>
       </form>
     );
