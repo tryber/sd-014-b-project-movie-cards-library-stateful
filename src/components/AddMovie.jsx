058fa88477be658fa9d64a0a5a74b994207724d9
Danilo <danilo.santossouza@outlook.com>
2021-09-08 11:22:31 -0300

    requisito 11 resolvido

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,40 +1,67 @@
 // implement AddMovie component here
 import React from 'react';
-import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
   constructor(props) {
     super(props);
+
+    this.handleChange = this.handleChange.bind(this);
 
     this.state = {
       subtitle: '',
       title: '',
       imagePath: '',
       storyline: '',
-      rating: 0,
-      genre: 'action',
+      // rating: 0,
+      // genre: 'action',
     };
   }
 
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { title, subtitle, imagePath, storyline } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label htmlFor="title-input" data-testid="title-input-label">
+        <label htmlFor="title-title" data-testid="title-input-label">
           Título
           <input
             data-testid="title-input"
-            id="text-input"
+            id="text-title"
             type="text"
-            value={ this.state.title }
+            value={ title }
+            onChange={ this.handleChange }
           />
         </label>
-        <label htmlFor="title-input" data-testid="subtitle-input-label">
+        <label htmlFor="title-subtitle" data-testid="subtitle-input-label">
           Subtítulo
           <input
-            data-testid="title-input"
-            id="text-input"
+            data-testid="subtitle-input"
+            id="text-subtitle"
             type="text"
-            value={ this.state.subtitle }
+            value={ subtitle }
+          />
+        </label>
+        <label htmlFor="title-image" data-testid="image-input-label">
+          Imagem
+          <input
+            data-testid="image-input"
+            id="text-subtitle"
+            type="text"
+            value={ imagePath }
+          />
+        </label>
+        <label htmlFor="title-image" data-testid="storyline-input-label">
+          Sinopse
+          <textarea
+            data-testid="storyline-input"
+            value={ storyline }
           />
         </label>
       </form>
