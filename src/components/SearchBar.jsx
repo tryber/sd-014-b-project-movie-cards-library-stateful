f2c0321be9e11b5fe2f1e2e52f6502ea48480c0c
Mateus Gambaro <magambaro@hotmail.com>
2021-09-08 14:57:21 -0300

    Adiciona SearchBar e os estados referentes as props recebidas

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,44 @@
 // implement SearchBar component here
+import React, { Component } from 'react'
+
+export class SearchBar extends Component {
+
+  constructor() {
+    super();
+
+    this.onSearchTextChange = this.onSearchTextChange.bind(this); // Usa-se para uma callback;
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+  }
+
+  onSearchTextChange({ target }) {
+    const { value } = target;
+    this.setState({ searchText: value });
+  }
+
+  onBookmarkedChange({ target }) {
+    const { checked } = target;
+    this.setState({ bookmarkedOnly: checked });
+  }
+
+  onSelectedGenreChange({ target }) {
+    const { value } = target;
+    this.setState({ selectedGenre: value });
+  }
+
+  render() {
+    return (
+      <form>
+
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
