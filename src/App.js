9d725df5a9f933f94ef7fb38f60fdcb97522be15
Lucas Alves <lucalves.rodrigues@gmail.com>
2021-09-08 17:23:32 -0300

    Requisito do 1 ao 5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
+import movies from './data';
 import './App.css';
+import MovieList from './components/MovieList';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar searchProps={ movies } />
+      <MovieList movies={ movies } />
     </div>
   );
 }
