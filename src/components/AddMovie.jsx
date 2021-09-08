34667d94d0f63b4828d998df47b55143a33b24c3
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2021-09-08 04:24:23 -0300

    Requisito 8 OK

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -14,14 +14,34 @@ class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { value, name } }) {
+    this.setState({
+      [name]: value,
+    });
   }
 
   render() {
     const { onClick } = this.props;
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
 
     return (
       <form data-testid="add-movie-form">
-        { onClick }
+        { onClick + subtitle + imagePath + storyline + rating + genre }
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            id="title-input"
+            name="title"
+            data-testid="title-input"
+            type="text"
+            value={ title }
+            onChange={ this.handleChange }
+          />
+        </label>
       </form>
     );
   }
