d52bad5181137cffef2004c1e20cb49b6f30aeb7
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2021-09-08 08:26:25 -0300

    Requisitos 16 e 17 OK

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
 import '../App.css';
 import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: [],
+    };
+  }
+
+  changer(keyName, { target }) {
+    this.setState({
+      [keyName]: target.value,
+    });
+  }
+
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+
     return (
       <div>
-        Renderiza danado
+        { movies }
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ (event) => this.changer('searchText', event) }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ (event) => this.changer('bookmarkedOnly', event) }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ (event) => this.changer('selectedGenre', event) }
+        />
       </div>
     );
   }
