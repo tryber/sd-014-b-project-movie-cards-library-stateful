a90c871830dc98a3d92118090d2076b2e0bc3d78
Rafael Veiga <rafaelveig@hotmail.com>
2021-09-08 14:57:06 -0300

    requisito 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,19 @@
 // implement SearchBar component here
-import React from 'react';
+import React from "react";
 
 class SearchBar extends React.Component {
   render() {
     return (
-      <div> teste </div>
+      <div>
+        <form data-testid="search-bar-form"> Teste</form>
+      </div>
     );
   }
 }
 
 export default SearchBar;
+
+/*
+Requisito 1: padrão, cria aqui e manda pro app.js
+Requisito 2: tag formulario com o atributo necessário
+*/
