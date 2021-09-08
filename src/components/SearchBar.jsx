d395f54a35aa370cf81130ae707c19f5a5afe5ce
enzo <enzo@pop-os.localdomain>
2021-09-08 09:03:36 -0300

    Requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -29,6 +29,20 @@ class SearchBar extends React.Component {
               data-testid="checkbox-input"
             />
           </label>
+          <label data-testid="select-input-label" htmlFor="select">
+            Filtrar por gênero
+            <select
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+              id="select"
+              data-testid="select-input"
+            >
+              <option data-testid="select-option" value=""> Todos </option>
+              <option data-testid="select-option" value="action"> Ação </option>
+              <option data-testid="select-option" value="comedy"> Comédia </option>
+              <option data-testid="select-option" value="thriller"> Suspense </option>
+            </select>
+          </label>
         </form>
       </div>);
   }
