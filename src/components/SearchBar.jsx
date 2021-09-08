983df2c5b7c13aa2c14cc67f876bf3cdf0aa89f5
Rafael Veiga <rafaelveig@hotmail.com>
2021-09-08 15:31:12 -0300

    requisito 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,9 @@
 // implement SearchBar component here
-import React from "react";
+import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly } = this.props;
     return (
       <form data-testid="search-bar-form">
         <label htmlFor="text-input" data-testid="text-input-label">
@@ -13,6 +13,16 @@ class SearchBar extends React.Component {
             data-testid="text-input"
             onChange={ onSearchTextChange }
             value={ searchText }
+          />
+        </label>
+        <label htmlFor="chekbox-input" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input 
+            type="checkbox"
+            name="bookmarkedOnly"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
           />
         </label>
       </form>
@@ -29,4 +39,5 @@ Requisito 3: cria-se a tag formulário
 cria-se o rótulo(label) com as atributos necessários
 rótulo do label 'inclui o texto' obrigatório
 cria-se o input com os requisitos necessários
+Requisito 4: 
 */
