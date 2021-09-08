8484815a05af640a67947c2d634c8ca1a801349c
lucas <mr.pine97@gmail.com>
2021-09-08 19:26:44 -0300

    refact: componentizando o input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,27 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import TextInput from './TextInput';
 
 export default class SearchBar extends Component {
   render() {
     const { searchText,
       onSearchTextChange,
-      // bookmarkedOnly,
-      // onBookmarkedChange,
-      // selectedGenre,
-      // onSelectedGenreChange
     } = this.props;
+    /*
+    const { searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange
+    } = this.props;
+    */
     return (
       <div>
         <form data-testid="search-bar-form">
-          <label data-testid="text-input-label" htmlFor="text-input">
-            Inclui o texto:
-            <input
-              type="text"
-              value={ searchText }
-              onChange={ onSearchTextChange }
-              data-testid="text-input"
-            />
-          </label>
+          <TextInput value={ searchText } onChange={ onSearchTextChange } />
         </form>
       </div>
     );
@@ -31,8 +29,12 @@ export default class SearchBar extends Component {
 SearchBar.propTypes = {
   searchText: PropTypes.string.isRequired,
   onSearchTextChange: PropTypes.func.isRequired,
-  // bookmarkedOnly: PropTypes.bool.isRequired,
-  // onBookmarkedChange: PropTypes.func.isRequired,
-  // selectedGenre: PropTypes.string.isRequired,
-  // onSelectedGenreChange: PropTypes.func.isRequired,
 };
+/*
+SearchBar.propTypes = {
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+*/
---
+++ src/components/TextInput.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class TextInput extends Component {
+  render() {
+    const { value,
+      onChange,
+    } = this.props;
+    return (
+      <div>
+        <label data-testid="text-input-label" htmlFor="text-input">
+          Inclui o texto:
+          <input
+            type="text"
+            value={ value }
+            onChange={ onChange }
+            data-testid="text-input"
+          />
+        </label>
+      </div>
+    );
+  }
+}
+
+TextInput.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
