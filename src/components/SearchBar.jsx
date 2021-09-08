0ce624bbd52a710fbd16a4edcce332be8480614e
Daniel <daniel.acoelho@outlook.com>
2021-09-08 16:11:54 -0300

    Requisitos 1 e 2com correção

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,17 +11,17 @@ class SearchBar extends React.Component {
       <form data-testid="search-bar-form">
 
       </form>
-    )
+    );
   }
 }
 
-SearchBar.propTypes = {
-  searchText: PropTypes.string.isRequired,
-  onSearchTextChange: PropTypes.func.isRequired,
-  bookmarkedOnly: PropTypes.bool.isRequired,
-  onBookmarkedChange: PropTypes.func.isRequired,
-  selectedGenre: PropTypes.string.isRequired,
-  onSelectedGenreChange: PropTypes.func.isRequired,
-};
+// SearchBar.propTypes = {
+//   searchText: PropTypes.string.isRequired,
+//   onSearchTextChange: PropTypes.func.isRequired,
+//   bookmarkedOnly: PropTypes.bool.isRequired,
+//   onBookmarkedChange: PropTypes.func.isRequired,
+//   selectedGenre: PropTypes.string.isRequired,
+//   onSelectedGenreChange: PropTypes.func.isRequired,
+// };
 
 export default SearchBar;
