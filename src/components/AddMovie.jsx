e8cfa6f4ee7baf18f4ac58c76ed533861308ed9e
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-09-08 14:47:33 -0400

    Resolver erro numero de linhas

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,87 +12,44 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
-    this.movieInformation = this.movieInformation.bind(this);
+    this.information = this.information.bind(this);
   }
 
-  movieInformation = (event) => {
-    const { name } = event.target;
-    const value = event.target.value;
+  information = ({ target }) => {
+    const { name, value } = target;
     this.setState({
       [name]: value,
     });
   }
 
-  // eslint-disable-next-line max-lines-per-function
   render() {
     const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
       <form>
         <label htmlFor="subtitle" data-testid="subtitle-input-label">
           Subtítulo
-          <input
-            type="text"
-            name="subtitle"
-            id="subtitle"
-            onChange={ this.movieInformation }
-            value={ subtitle }
-            data-testid="subtitle-input"
-          />
+          <input type="text" name="subtitle" onChange={ this.information } value={ subtitle } data-testid="subtitle-input"/>
         </label>
         <label htmlFor="title" data-testid="title-input-label">
           Título
-          <input
-            type="text"
-            name="title"
-            id="title"
-            onChange={ this.movieInformation }
-            value={ title }
-            data-testid="title-input"
-          />
+          <input type="text" name="title" onChange={ this.information } value={ title } data-testid="title-input"/>
         </label>
         <label htmlFor="imagePath" data-testid="image-input-label">
-          caminho da imagem
-          <input
-            type="text"
-            name="imagePath"
-            id="imagePath"
-            onChange={ this.movieInformation }
-            value={ imagePath }
-            data-testid="image-input"
-          />
+          Imagem
+          <input type="text" name="imagePath" onChange={ this.information } value={ imagePath } data-testid="image-input" />
         </label>
         <label htmlFor="storyline" data-testid="storyline-input-label">
-          sinopse
-          <input
-            name="storyline"
-            type="text"
-            id="storyline"
-            onChange={ this.movieInformation }
-            value={ storyline }
-            data-testid="storyline-input"
-          />
+          Sinopse
+          {/* <textarea name="" id="" cols="30" rows="10"></textarea> */}
+          <input name="storyline" type="text" onChange={ this.information } value={ storyline } data-testid="storyline-input"/>
         </label>
         <labe htmlFor="rating" data-testid="rating-input-label">
-          avaliação
-          <input
-            name="rating"
-            type="number"
-            id="rating"
-            onChange={ this.movieInformation }
-            value={ rating }
-            data-testid="rating-input"
-          />
+          Avaliação
+          <input name="rating" type="number" onChange={ this.information } value={ rating } data-testid="rating-input" />
         </labe>
         <label htmlFor="genre" data-testid="genre-input-label">
           gênero
-          <input
-            name="genre"
-            type="text"
-            id="genre"
-            onChange={ this.movieInformation }
-            value={ genre }
-            data-testid="genre-input"
-          />
+          <input name="genre" type="text" onChange={ this.information } value={ genre } data-testid="genre-input"/>
         </label>
       </form>
     );
