4520b7b31773193d16217f705c925882b814ca77
Elton Rodrigues <el_dantas1999@hotmail.com>
2021-09-08 16:40:47 -0300

    Requisito 05 - Corrigido

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export class SearchBar extends React.Component {
+class SearchBar extends React.Component {
   render() {
     const { searchText,
       onSearchTextChange,
