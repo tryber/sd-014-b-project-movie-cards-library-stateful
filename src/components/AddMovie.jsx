9d5e2912ae46c8dbae4b7380bbe78709b3c8cefd
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-09-08 14:29:07 -0400

    requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
   constructor() {
@@ -21,10 +21,12 @@ class AddMovie extends React.Component {
     });
   }
 
+  // eslint-disable-next-line max-lines-per-function
   render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
       <form>
-        <label htmlFor="subtitle">
+        <label htmlFor="subtitle" data-testid="subtitle-input-label">
           subtítulo
           <input
             type="text"
@@ -35,13 +37,64 @@ class AddMovie extends React.Component {
             data-testid="subtitle-input"
           />
         </label>
+        <label htmlFor="title" data-testid="title-input-label">
+          título
+          <input
+            type="text"
+            name="title"
+            id="title"
+            onChange={ this.movieInformation }
+            value={ title }
+            data-testid="title-input"
+          />
+        </label>
+        <label htmlFor="imagePath" data-testid="image-input-label">
+          caminho da imagem
+          <input
+            type="text"
+            name="imagePath"
+            id="imagePath"
+            onChange={ this.movieInformation }
+            value={ imagePath }
+            data-testid="image-input"
+          />
+        </label>
+        <label htmlFor="storyline" data-testid="storyline-input-label">
+          sinopse
+          <input
+            name="storyline"
+            type="text"
+            id="storyline"
+            onChange={ this.movieInformation }
+            value={ storyline }
+            data-testid="storyline-input"
+          />
+        </label>
+        <labe htmlFor="rating" data-testid="rating-input-label">
+          avaliação
+          <input
+            name="rating"
+            type="number"
+            id="rating"
+            onChange={ this.movieInformation }
+            value={ rating }
+            data-testid="rating-input"
+          />
+        </labe>
+        <label htmlFor="genre" data-testid="genre-input-label">
+          gênero
+          <input
+            name="genre"
+            type="text"
+            id="genre"
+            onChange={ this.movieInformation }
+            value={ genre }
+            data-testid="genre-input"
+          />
+        </label>
       </form>
     );
   }
 }
-
-AddMovie.propTypes = {
-  subtitle: PropTypes.string.isRequired,
-};
 
 export default AddMovie;
