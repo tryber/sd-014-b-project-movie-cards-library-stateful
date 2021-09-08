39137eaba096ca29528ee3c5e7be89685cc2f18b
Ronaldo Junior <ronaldosenajr@gmail.com>
2021-09-08 15:38:04 -0400

    Requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,31 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { onClick } = this.props;
+    return (
+      <form action="" />
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onclick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
