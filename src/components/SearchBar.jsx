4f806c1dee2bb71ba6eede14564081dc58c99131
Rafael Veiga <rafaelveig@hotmail.com>
2021-09-08 15:18:28 -0300

    requisito 3 completo Inclui o texto eh obrigarotio

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,7 +7,7 @@ class SearchBar extends React.Component {
     return (
       <form data-testid="search-bar-form">
         <label htmlFor="text-input" data-testid="text-input-label">
-          TESTE
+          Inclui o texto
           <input
             type="text"
             data-testid="text-input"
@@ -25,4 +25,8 @@ export default SearchBar;
 /*
 Requisito 1: padrão, cria aqui e manda pro app.js
 Requisito 2: tag formulario com o atributo necessário
+Requisito 3: cria-se a tag formulário
+cria-se o rótulo(label) com as atributos necessários
+rótulo do label 'inclui o texto' obrigatório
+cria-se o input com os requisitos necessários
 */
