e938897db21b1483cfc87477d6fa182ea203d90d
belaguarino <izabelaguarino@gmail.com>
2021-09-08 09:36:56 -0300

    Requisitos 15 a 19

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,83 @@
-// implement MovieLibrary component here
+// Requisito 15
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+
+export default class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+
+    this.handleSearchText = this.handleSearchText.bind(this);
+    this.handleBookmarkedOnly = this.handleBookmarkedOnly.bind(this);
+    this.handleSelectedGenre = this.handleSelectedGenre.bind(this);
+  }
+
+  // Requisito 16
+  // handles feitos com a ajuda de Guilherme Andrade
+  handleSearchText({ target }) {
+    this.setState({
+      searchText: target.value,
+    });
+  }
+
+  handleBookmarkedOnly({ target }) {
+    this.setState({
+      bookmarkedOnly: target.checked,
+    });
+  }
+
+  handleSelectedGenre({ target }) {
+    this.setState({
+      selectedGenre: target.value,
+    });
+  }
+
+  handleAddMovies(newMovie) {
+    this.setState((previousState) => ({
+      movies: [...previousState.movies, newMovie],
+    }));
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    return (
+      <div>
+        {/* Requisito 17 */}
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.handleSearchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.handleBookmarkedOnly }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.handleSelectedGenre }
+        />
+        {/* Requisito 18 - feito com ajuda de Guilherme Andrade (ternário) */}
+        <MovieList
+          movies={ movies.filter((movie) => (
+            (bookmarkedOnly === false ? true : movie.bookmarked === true)
+              && (movie.genre.includes(selectedGenre))
+              && (
+                (movie.title.includes(searchText))
+                || (movie.subtitle.includes(searchText))
+                || (movie.storyline.includes(searchText))))) }
+        />
+        {/* Requisito 19 */}
+        <AddMovie onClick={ (newMovie) => this.handleAddMovies(newMovie) } />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
