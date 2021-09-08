307919f65dd520e8b7b7cff1a2cf8b22496afcbb
belaguarino <izabelaguarino@gmail.com>
2021-09-08 08:53:09 -0300

    Requisito 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,12 @@
 // 1) Criando o SearchBar
+import React from 'react';
+
+// 1 - Crie um componente chamado <SearchBar />
+
+export default class SearchBar extends React.Component {
+  render() {
+      return(
+          // Inserir o elemento a ser retornado
+      )
+  }
+}
