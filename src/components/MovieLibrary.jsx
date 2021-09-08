b4923d3b118094867d6e793f68683a854baa58cf
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2021-09-08 07:54:25 -0300

    Requisito 15 OK

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,19 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+import '../App.css';
+import PropTypes from 'prop-types';
+
+class MovieLibrary extends Component {
+  render() {
+    return (
+      <div>
+        Renderiza danado
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.array,
+}.isRequired;
+
+export default MovieLibrary;
