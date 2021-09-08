ed71099f1274b17bef0c49afc37fb694129db214
Rafael Veiga <rafaelveig@hotmail.com>
2021-09-08 17:21:49 -0300

    req 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -17,9 +17,17 @@ class AddMovie extends React.Component {
   render() {
     const { onclick } = this.props;
     return (
-      <form action="" />
+      <form action="" data-testid="add-movie-form" />
     );
   }
 }
 
 export default AddMovie;
+
+/**
+Requisito 6: devemos setar o estado inicial, com isso abrimos o constructor recebendo props
+super para alterar o padrão recebendo as props
+setamos o valor inicial dentro do this.state
+dentro do render fazemos o formulário com a callback onclick ja no ponto de uso.
+Requisito 7: só adicionar o atributo
+*/
