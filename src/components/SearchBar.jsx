0a6be41fede246a41eeb51d57096b2bf470ea444
Daniel <daniel.acoelho@outlook.com>
2021-09-08 18:37:15 -0300

    Requisitos 1 a 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,19 +3,39 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange } = this.props;
-    // bookmarkedOnly, onBookmarkedChange,
-    // selectedGenre, onSelectedGenreChange
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
+        selectedGenre, onSelectedGenreChange } = this.props;
+    
     return (
       <form data-testid="search-bar-form">
         <section>
-          <label htmlFor="searchBar" data-testid="text-input-label">
+          <label data-testid="text-input-label">
             Inclui o texto:
             <input
               type="text"
               data-testid="text-input"
               value={ searchText }
               onChange={ onSearchTextChange } />
+          </label>
+          <label data-testid="checkbox-input-label">
+              Mostrar somente favoritos
+              <input
+                data-testid="checkbox-input"
+                type="checkbox"
+                checked={ bookmarkedOnly }
+                onChange={ onBookmarkedChange } />
+          </label>
+          <label data-testid="select-input-label">
+            Filtrar por gênero
+            <select
+              data-testid="select-input"
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }>
+              <option data-testid="select-option" value="">Todos</option>
+              <option data-testid="select-option" value="action">Ação</option>
+              <option data-testid="select-option" value="comedy">Comédia</option>
+              <option data-testid="select-option" value="thriller">Suspense</option>
+            </select>    
           </label>
         </section>  
       </form>
