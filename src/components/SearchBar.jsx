b7e4fbaa543bd237020d064b3977013457c1fced
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2021-09-08 11:47:01 -0300

    Requisito 1 :heavy_check_mark:

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,11 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div>Hii</div>
+    );
+  }
+}
+
+export default SearchBar;
