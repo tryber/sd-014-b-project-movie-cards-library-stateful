5d57fa49bc74df6b5d1c87f455407cb53370d5a2
igor.marinho <igormarinhoargollo@gmail.com>
2021-09-08 16:12:41 -0300

    Movie Cards Library Stateful Project - Requeriments: 1, 2, 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,2 +1,22 @@
 // implement SearchBar component here
-// ajuste do PR
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+    } = this.props;
+
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-input" data-testid="text-input-label">
+          Inclui o texto:
+          <input value={ searchText } onChange={ onSearchTextChange } data-testid="text-input" />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
