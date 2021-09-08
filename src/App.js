cacb403486987f75477846e567f2bbe801251051
LucasAccurcio <lucas.accurcio@gmail.com>
2021-09-07 23:38:51 -0300

    Correcoes Lint

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar />
+      <SearchBar movies={ movies } />
       <MovieList movies={ movies } />
       <AddMovie />
     </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <div className="header-container">
         <label className="header-label" htmlFor="find-text">
           Inclui o texto:
           <input className="header-input" id="find-text" type="text" />
         </label>
-
         <label className="header-label" htmlFor="show-only-favorites">
           <input
             className="input-checkbox"
@@ -18,17 +19,27 @@ class SearchBar extends React.Component {
           />
           Mostrar somente favoritos
         </label>
-
         <label className="header-label" htmlFor="filter-genre">
           Filtrar por gênero:
           <select className="header-input" id="filter-genre">
             <option value="">Selecione um filtro</option>
-            <option value="action">Ação</option>
+            {movies
+              .map(({ genre }, index) => (
+                <option
+                  key={ index + 1 }
+                  value="action"
+                >
+                  { genre }
+                </option>))}
           </select>
         </label>
       </div>
     );
   }
 }
+
+SearchBar.propTypes = {
+  movies: PropTypes.string.isRequired,
+};
 
 export default SearchBar;
