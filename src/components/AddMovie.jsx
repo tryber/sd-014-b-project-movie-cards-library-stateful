d52d317ffb5b5aee6d31aae3684af3967be256fc
Davisralves <davialves324@gmail.com>
2021-09-08 15:41:18 -0300

    finalizando requisito 13

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -70,6 +70,19 @@ class AddMovie extends React.Component {
             onChange={ this.handleClick }
           />
         </label>
+        <label data-testid="genre-input-label" htmlFor="genre">
+          Gênero
+          <select
+            name="genre"
+            data-testid="genre-input"
+            value={ genre }
+            onChange={ this.handleClick }
+          >
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
