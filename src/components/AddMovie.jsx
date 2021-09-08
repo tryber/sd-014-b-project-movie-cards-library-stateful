42127886083f68b158f34e896223969088b48cd6
enzo <enzo@pop-os.localdomain>
2021-09-08 09:14:45 -0300

    Requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,28 @@
 // implement AddMovie component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle:'',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { onClick } = this.props;
+    return <h1> AddMovie </h1>;
+  }
+}
+
+// AddMovie.propTypes = {
+
+// }
+
+export default AddMovie;
