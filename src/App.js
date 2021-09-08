76e13f27f1ab11ee7daebea25f0c3247ca690e70
LucasAccurcio <lucas.accurcio@gmail.com>
2021-09-08 19:07:21 -0300

    Requisito 15

--- src/App.js
+++ src/App.js
@@ -1,44 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
-import MovieList from './components/MovieList';
 import movies from './data';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
 import './App.css';
+import MovieLibrary from './components/MovieLibrary';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      searchText: '',
-      bookmarkedOnly: false,
-      selectedGenre: '',
-    };
-  }
-
-  addNewMovie() {
-
-  }
-
   render() {
-    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
-
     return (
       <div className="App">
         <Header />
-
-        <SearchBar
-          searchText={ searchText }
-          onSearchTextChange={ this.onSearchTextChange }
-          bookmarkedOnly={ bookmarkedOnly }
-          onBookmarkedChange={ this.onBookmarkedChange }
-          selectedGenre={ selectedGenre }
-          onSelectedGenreChange={ this.onSelectedGenreChange }
-        />
-
-        <MovieList movies={ movies } />
-
-        <AddMovie onClick={ this.addNewMovie } />
+        <MovieLibrary movies={ movies } />
       </div>
     );
   }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,42 @@
-// implement MovieLibrary component here
+import React from 'react';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+  }
+
+  addNewMovie() {
+
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    const { movies } = this.props;
+    return (
+      <div className="App">
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+
+        <MovieList movies={ movies } />
+
+        <AddMovie onClick={ this.addNewMovie } />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
