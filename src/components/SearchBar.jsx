d67eb39dd140d6344e30826491062023fde65fb6
lucas <mr.pine97@gmail.com>
2021-09-08 18:49:45 -0300

    impl: teste para ver o avaliador do github

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,10 +5,11 @@ export default class SearchBar extends Component {
   render() {
     const { searchText,
       onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange } = this.props;
+      // bookmarkedOnly,
+      // onBookmarkedChange,
+      // selectedGenre,
+      // onSelectedGenreChange
+    } = this.props;
     return (
       <div>
         <form data-testid="search-bar-form">
@@ -30,8 +31,8 @@ export default class SearchBar extends Component {
 SearchBar.propTypes = {
   searchText: PropTypes.string.isRequired,
   onSearchTextChange: PropTypes.func.isRequired,
-  bookmarkedOnly: PropTypes.bool.isRequired,
-  onBookmarkedChange: PropTypes.func.isRequired,
-  selectedGenre: PropTypes.string.isRequired,
-  onSelectedGenreChange: PropTypes.func.isRequired,
+  // bookmarkedOnly: PropTypes.bool.isRequired,
+  // onBookmarkedChange: PropTypes.func.isRequired,
+  // selectedGenre: PropTypes.string.isRequired,
+  // onSelectedGenreChange: PropTypes.func.isRequired,
 };
