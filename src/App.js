b256c161c878706e9c05304171ed516a50d4ee83
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-09-08 13:19:17 -0400

    commit depois do pull

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 import movies from './data';
+import AddMovie from './components/AddMovie';
 import './App.css';
 import SearchBar from './components/SearchBar';
 
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <SearchBar />
       <MovieList movies={ movies } />
+      <AddMovie />
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,47 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.movieInformation = this.movieInformation.bind(this);
+  }
+
+  movieInformation = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <label htmlFor="subtitle">
+          subtítulo
+          <input
+            type="text"
+            name="subtitle"
+            id="subtitle"
+            onChange={ this.movieInformation }
+            value={ subtitle }
+            data-testid="subtitle-input"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  subtitle: PropTypes.string.isRequired,
+};
+
+export default AddMovie;
