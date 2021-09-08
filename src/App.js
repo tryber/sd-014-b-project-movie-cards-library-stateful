5f720e8b83e680ec5e52e2de0e4c76f31ea0d140
Bruno M Landim <brunolandim13@hotmail.com>
2021-09-08 18:00:07 -0300

    Requisitos 1 2 e 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import MovieList from './components/MovieList';
+import Data from './data';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
+      <MovieList movies={ Data } />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,40 @@
-// implement SearchBar component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="texto">
+          Inclui o texto
+          <input
+            data-testid="text-input"
+            id="texto"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+        <input
+          type="checkbox"
+        />
+      </form>
+    );
+  }
+}
+SearchBar.protoTypes = {
+  searchText : PropTypes.string,
+  onSearchTextChange : PropTypes.string,
+  bookmarkedOnly : PropTypes.string,
+  onBookmarkedChange : PropTypes.string,
+  selectedGenre : PropTypes.string,
+  onSelectedGenreChange : PropTypes.string,
+};
+
+export default SearchBar;
