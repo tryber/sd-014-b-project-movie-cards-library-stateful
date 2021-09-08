be9f913241aae8ec504f08218827cb6680bbce2a
vanim <vanimouzad123@gmail.com>
2021-09-08 14:21:34 -0300

    commit para testar avaliador

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,10 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return console.log(this.props);
+  }
+}
+
+export default SearchBar;
