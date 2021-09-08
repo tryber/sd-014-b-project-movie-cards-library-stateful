23e48c107fc7916ff303364e7f5d1b452740b4cc
Rafael Moraes - Turma 14 - Tribo B <rafael1moraes1rocha@gmail.com>
2021-09-08 14:52:08 -0300

    [Rafael Moraes] Projeto Movie Cards Library Stateful :clapper: :movie_camera:

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <>
+      <header className="App">
+        <Header />
+      </header>
+      <main>
+        <SearchBar
+          searchtext=""
+          onSearchTextChange=""
+          bookmarkedOnly=""
+          selectedGenre
+          onSelectedGenreChange
+        />
+      </main>
+    </>
+
   );
 }
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,22 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>SearchBar</h2>
+        <input placeholder="Searchbar maravilhoso" />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
