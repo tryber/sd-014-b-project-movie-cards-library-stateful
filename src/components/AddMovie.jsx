f7b7eb0814d4eb335222a3bb6496ce751891b4cd
andremorsch <andremorsch37@gmail.com>
2021-09-08 15:48:27 -0300

    npm

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,27 @@
-// implement AddMovie component here abcd
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  reder() {
+    const { onClick } = this.props;
+
+    return (
+      <div>
+        asdf
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
