41be18f7408e35ea7128a98be75c2feab966eb08
Rafael Veiga <rafaelveig@hotmail.com>
2021-09-08 16:45:46 -0300

    requisito 5 completo

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,17 @@
 // implement SearchBar component here
-import React from 'react';
+import React from "react";
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly } = this.props;
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
     return (
       <form data-testid="search-bar-form">
         <label htmlFor="text-input" data-testid="text-input-label">
@@ -11,19 +19,32 @@ class SearchBar extends React.Component {
           <input
             type="text"
             data-testid="text-input"
-            onChange={ onSearchTextChange }
-            value={ searchText }
+            onChange={onSearchTextChange}
+            value={searchText}
           />
         </label>
         <label htmlFor="chekbox-input" data-testid="checkbox-input-label">
           Mostrar somente favoritos
-          <input 
+          <input
             type="checkbox"
             name="bookmarkedOnly"
-            checked={ bookmarkedOnly }
-            onChange={ onBookmarkedChange }
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
             data-testid="checkbox-input"
           />
+        </label>
+        <label htmlFor="select-input" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
         </label>
       </form>
     );
@@ -39,5 +60,6 @@ Requisito 3: cria-se a tag formulário
 cria-se o rótulo(label) com as atributos necessários
 rótulo do label 'inclui o texto' obrigatório
 cria-se o input com os requisitos necessários
-Requisito 4: 
+Requisito 4: quase igual o anterior só que com checkbox
+Requisito5: 
 */
