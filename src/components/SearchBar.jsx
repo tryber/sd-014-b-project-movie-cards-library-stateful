09ead5e46675d5464a9a84f4a1c9c928120b790f
Rafael Moraes - Turma 14 - Tribo B <rafael1moraes1rocha@gmail.com>
2021-09-08 15:18:48 -0300

    [Rafael Moraes] Projeto Movie Cards Library Stateful :clapper: :movie_camera: - Requisitos 2 e 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,10 +10,25 @@ class SearchBar extends React.Component {
   }
 
   render() {
+    const { searchText, onSearchTextChange } = this.props;
+    console.log(this);
+
     return (
       <div>
         <h2>SearchBar</h2>
-        <input placeholder="Searchbar maravilhoso" />
+        <form data-testid="search-bar-form">
+          <label htmlFor="input" data-testid="text-input-label">
+            Inclui o texto
+            <input
+              name="input"
+              placeholder="Searchbar maravilhoso"
+              value={ searchText }
+              onChange={ onSearchTextChange }
+              data-testid="text-input"
+            />
+          </label>
+
+        </form>
       </div>
     );
   }
