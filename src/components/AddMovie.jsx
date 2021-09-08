216169eef1ad2b881a4a7403ab9c214780373ecf
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-09-08 15:14:02 -0400

    Correçao linter

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -29,32 +29,60 @@ class AddMovie extends React.Component {
         <label htmlFor="subtitle" data-testid="subtitle-input-label">
           Subtítulo
           <input
-          type="text"
-          name="subtitle"
-          onChange={ this.information }
-          value={ subtitle }
-          data-testid="subtitle-input" />
+            type="text"
+            name="subtitle"
+            onChange={ this.information }
+            value={ subtitle }
+            data-testid="subtitle-input"
+          />
         </label>
         <label htmlFor="title" data-testid="title-input-label">
           Título
-          <input type="text" name="title" onChange={ this.information } value={ title } data-testid="title-input" />
+          <input
+            type="text"
+            name="title"
+            onChange={ this.information }
+            value={ title }
+            data-testid="title-input"
+          />
         </label>
         <label htmlFor="imagePath" data-testid="image-input-label">
           Imagem
-          <input type="text" name="imagePath" onChange={ this.information } value={ imagePath } data-testid="image-input" />
+          <input
+            type="text"
+            name="imagePath"
+            onChange={ this.information }
+            value={ imagePath }
+            data-testid="image-input"
+          />
         </label>
         <label htmlFor="storyline" data-testid="storyline-input-label">
           Sinopse
-          {/* <textarea name="" id="" cols="30" rows="10"></textarea> */}
-          <input name="storyline" type="text" onChange={ this.information } value={ storyline } data-testid="storyline-input"/>
+          <textarea
+            data-testid="storyline-input"
+            name="storyline"
+            type="text"
+            onChange={ this.information }
+            value={ storyline }
+          ></textarea>
         </label>
-        <labe htmlFor="rating" data-testid="rating-input-label">
+        <label htmlFor="rating" data-testid="rating-input-label">
           Avaliação
-          <input name="rating" type="number" onChange={ this.information } value={ rating } data-testid="rating-input" />
-        </labe>
+          <input
+            name="rating"
+            type="number"
+            onChange={ this.information }
+            value={ rating }
+            data-testid="rating-input"
+          />
+        </label>
         <label htmlFor="genre" data-testid="genre-input-label">
-          gênero
-          <input name="genre" type="text" onChange={ this.information } value={ genre } data-testid="genre-input"/>
+          Gênero
+          <select data-testid="genre-input" onChange={ this.information } value={ genre }>
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+          </select>
         </label>
       </form>
     );
