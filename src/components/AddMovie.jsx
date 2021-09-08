d8db56aa0f1c939d4d167c849d8c22fa0654da77
Mateus Gambaro <magambaro@hotmail.com>
2021-09-08 16:01:03 -0300

    Cria um componente AddMovie e adiciona um formulario dentro

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,30 @@
 // implement AddMovie component here
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
+export class AddMovie extends Component {
+constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { onClick } = this.props;
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
+
+    return (
+      <form data-testid="add-movie-form">
+                
+      </form>
+    )
+}
+}
+
+export default AddMovie
