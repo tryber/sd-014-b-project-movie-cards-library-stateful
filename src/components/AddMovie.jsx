6affc1679608f180398aa4710a643b6b06496181
Ronaldo Junior <ronaldosenajr@gmail.com>
2021-09-08 16:00:12 -0400

    Requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -6,6 +6,8 @@ class AddMovie extends React.Component {
   constructor(props) {
     super(props);
 
+    this.handleText = this.handleText.bind(this);
+
     this.state = {
       subtitle: '',
       title: '',
@@ -16,16 +18,35 @@ class AddMovie extends React.Component {
     };
   }
 
+  handleText(event) {
+    const { value } = event.target;
+    this.setState(() => ({
+      title: value,
+    }));
+  }
+
   render() {
     const { onClick } = this.props;
+    const { title } = this.state;
+
     return (
-      <form action="" data-testid="add-movie-form" />
+      <form action="" data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.handleText }
+          />
+        </label>
+      </form>
     );
   }
 }
 
 AddMovie.propTypes = {
-  onclick: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default AddMovie;
