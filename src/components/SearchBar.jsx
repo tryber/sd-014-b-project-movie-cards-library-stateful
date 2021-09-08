ac8d8b1666526b3397f7f6507ac02f77ece4f53d
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-09-08 20:41:10 +0100

    Requisito 3 - renderiza input dentro do form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,7 +6,18 @@ class SearchBar extends Component {
       onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
 
     return (
-      <form data-testid="search-bar-form" />
+      <form data-testid="search-bar-form">
+        <label htmlFor="searchBar" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
+
+      </form>
     );
   }
 }
