87a798bc30ca171f640e77c5e3c26343b687d0e7
Rafael Veiga <rafaelveig@hotmail.com>
2021-09-08 17:48:40 -0300

    req 8 pela metade

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,26 +1,52 @@
 // implement AddMovie component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      subtitle: '',
+      // subtitle: '',
       title: '',
-      imagePath: '',
-      storyline: '',
-      rating: 0,
-      genre: 'action',
+      // imagePath: '',
+      // storyline: '',
+      // rating: 0,
+      // genre: 'action',
     };
   }
 
+  handleInputChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    const { onclick } = this.props;
+    // const { addMovie } = this.props;
+    const { title } = this.state;
     return (
-      <form action="" data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label htmlFor="subtitle" data-testid="title-input-label">
+          Título
+          <input
+            id="title"
+            name="title"
+            value={ title }
+            type="text"
+            data-testid="title-input"
+            onChange={ this.handleInputChange }
+          />
+        </label>
+      </form>
     );
   }
 }
+
+AddMovie.propTypes = {
+  addMovie: PropTypes.func.isRequired,
+};
 
 export default AddMovie;
 
@@ -30,4 +56,7 @@ super para alterar o padrão recebendo as props
 setamos o valor inicial dentro do this.state
 dentro do render fazemos o formulário com a callback onclick ja no ponto de uso.
 Requisito 7: só adicionar o atributo
+Requisito 8: para que haja iteração entre o texto digitado e o estado fazemos o handleInputChange
+que faz a verificaçao do checkbox primeiro
+depois cuida dinamicamente do nome e valor dos nossos atributos
 */
