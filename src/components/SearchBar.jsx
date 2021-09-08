6fb9d673bc87ec7de787fcf7a25901b43c73944d
Cecilia Cacais <ceci.cacais@gmail.com>
2021-09-08 16:05:24 -0300

    render select dentro do form do SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,7 @@ class SearchBar extends React.Component {
   render() {
     const { filme: { searchText, onSearchTextChange,
       bookmarkedOnly, onBookmarkedChange,
-      selectedGenre, onSelectedGenreChange, } } = this.props;
+      selectedGenre, onSelectedGenreChange } } = this.props;
     return (
       <form data-testid="search-bar-form">
         <label htmlFor="texto-um" data-testid="text-input-label">
@@ -27,6 +27,20 @@ class SearchBar extends React.Component {
             checked={ bookmarkedOnly }
             onChange={ onBookmarkedChange }
           />
+        </label>
+        <label htmlFor="select-um" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            id="select-um"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
         </label>
       </form>
     );
