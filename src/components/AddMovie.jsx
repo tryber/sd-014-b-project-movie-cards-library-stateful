580ac5ba170910215229598a61c04a6f2d19d112
icaroemanuel <icaro.emanuel@outlook.com>
2021-09-08 17:00:19 -0300

    Requisito 6-Criado o componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,27 @@
 // implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    return (
+      <section>
+        
+      </section>
+    );
+  }
+}
+
+export default AddMovie;
