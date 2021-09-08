8a953865cb96e49954143ca0d1701811f841af01
Nataniel Santos <nathan.santos159@hotmail.com>
2021-09-08 18:58:02 -0300

    Requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -32,9 +32,30 @@ class SearchBar extends React.Component {
             onChange={ onBookmarkedChange }
           />
         </label>
+        <label htmlFor="select-input" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            type="select"
+            value={ selectedGenre }
+            id="select-input"
+            data-testid="select-input"
+            onChange={ onSelectedGenreChange }
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+          
+        </label>
       </form>
     );
   }
 }
 
 export default SearchBar;
+
+Todos, com o valor "";
+Ação, com o valor action;
+Comédia, com o valor comedy;
+Suspense, com o valor thriller.
