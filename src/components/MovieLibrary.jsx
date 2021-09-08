65c91268b22e0e96d43eb4b564e6f844085ba0f8
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-09-08 16:49:15 -0300

    Requisitos 16 e 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { SearchBar } from './SearchBar';
 
 class MovieLibrary extends Component {
-  render() {
+  constructor() {
+    super();
+
     const { movies } = this.props;
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = (target.type === 'checkbox') ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
-      <div>
-        
-      </div>
+      <SearchBar
+        searchText={ searchText }
+        onSearchTextChange={ this.handleChange }
+        bookmarkedOnly={ bookmarkedOnly }
+        onBookmarkedChange={ this.handleChange }
+        selectedGenre={ selectedGenre }
+        onSelectedGenreChange={ this.handleChange }
+      />
     );
   }
 }
