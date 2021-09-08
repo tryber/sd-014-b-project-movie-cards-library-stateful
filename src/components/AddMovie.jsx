6a4c59510154d14c1b574ae6aeedc8b06700b4b7
Vitor Lima <vitorliman@gmail.com>
2021-09-08 16:32:26 -0300

    Finaliza requisito 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,45 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
+
+  render() {
+    /* const {
+      subtitle,
+      title,
+      imagePath,
+      storyline,
+      rating,
+      genre,
+    } = this.props; */
+
+    return (
+      <form data-testid="add-movie-form">
+
+      </form>
+    )
+  }
+}
+
+AddMovie.protoTypes = {
+  subtitle: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  imagePath: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  genre: PropTypes.string.isRequired,
+}
+
+export default AddMovie;
