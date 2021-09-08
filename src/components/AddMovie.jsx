da2c984745141395eba36b334eca2c72f7eeaeae
Cecilia Cacais <ceci.cacais@gmail.com>
2021-09-08 17:42:22 -0300

    cria componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,26 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+    constructor() {
+      super()
+
+      this.state = {
+        subtitle: '',
+        title: '',
+        imagePath: '',
+        storyline: '',
+        rating: 0,
+        genre: ''
+      };
+    };
+    render() {
+        return (
+
+        );
+    }
+}
+
+AddMovie.propTypes = {
+    onClick: PropTypes.func.isRequired,
+}
