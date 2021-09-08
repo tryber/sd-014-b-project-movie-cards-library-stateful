12d961f8eedeb62b59ef6c8e8ac923de479297dc
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-09-07 21:42:26 -0300

    requisitos do 1 ao 3: renderizando um componente, 1 form, e 1 input type text

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,35 @@
-// implement SearchBar component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const { SearchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="text">
+          Inclui o texto:
+          <input
+            data-testid="text-input"
+            onChange={ onSearchTextChange }
+            type="text"
+            id="text"
+            name="text"
+            value={ SearchText }
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  SearchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
