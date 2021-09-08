5d8e3af98ceabfeebcec922bd671e098011002cf
Elithon Silva <elithonsilva@gmail.com>
2021-09-08 17:41:36 -0300

    impl: componente SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,14 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+
+  render() {
+
+    const movies = { searchText, onSearchTextChange, bookmarkedOnly, onBookMarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+
+    return ()
+  }
+}
+
+export default SearchBar;
