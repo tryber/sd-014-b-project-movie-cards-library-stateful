bc9cdb0911a223f8374382ffbc2787876ff9daa7
Cecilia Cacais <ceci.cacais@gmail.com>
2021-09-08 15:03:46 -0300

    Implementa SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,24 @@
-// implement SearchBar component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const { filme } = this.props;
+    return (
+      <section>
+        { filme }
+      </section>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  filme: PropTypes.shape({
+    searchText: PropTypes.string,
+    onSearchTextChange: PropTypes.func,
+    bookmarkedOnly: PropTypes.bool,
+    onBookmarkedChange: PropTypes.func,
+    selectedGenre: PropTypes.string,
+    onSelectedGenreChange: PropTypes.func,
+  }).isRequired,
+};
