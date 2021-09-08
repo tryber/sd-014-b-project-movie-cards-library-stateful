c4187743d82c75b736bf4a0a485b765ec2d79c84
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-09-08 21:57:43 +0100

    Requisito 6 - cria componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,24 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    return (
+      <h1>Teste</h1>
+    );
+  }
+}
+
+export default AddMovie;
