9ef08e1aebc6e88062de79434353f7efc13d1bc2
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-09-08 16:37:26 -0300

    Requisito 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange
+    } = this.props;
     return (
-      <form>
-        
+      <form data-testid="search-bar-form">
+
       </form>
-    )
+    );
   }
 }
 
