51753dbd7718f26ae995be0a9ea45bb2635073bc
Israel <israel.fab@hotmail.com>
2021-09-08 18:25:51 -0300

    Requisito 10

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -9,7 +9,7 @@ class AddMovie extends React.Component {
     this.state = {
       subtitle: '',
       title: '',
-      // imagePath: '',
+      imagePath: '',
       // storyline: '',
       // rating: 0,
       // genre: 'action',
@@ -20,12 +20,13 @@ class AddMovie extends React.Component {
     this.setState({
       title: event.target.value,
       subtitle: event.target.value,
+      imagePath: event.target.value,
     });
   }
 
   render() {
     // const { onClick } = this.props;
-    const { title, subtitle } = this.state;
+    const { title, subtitle, imagePath } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="title-input" data-testid="title-input-label">
@@ -43,6 +44,15 @@ class AddMovie extends React.Component {
             type="text"
             value={ subtitle }
             data-testid="subtitle-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="image-input" data-testid="image-input-label">
+          Imagem
+          <input
+            type="text"
+            value={ imagePath }
+            data-testid="image-input"
             onChange={ this.handleChange }
           />
         </label>
