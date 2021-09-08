af89f97ba30af4e9b093d91db4a4c587b1e58345
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-09-08 16:46:12 -0300

    Requisito 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,7 +12,14 @@ class SearchBar extends React.Component {
     } = this.props;
     return (
       <form data-testid="search-bar-form">
-
+        <label htmlFor="Seach" data-testid="text-input-label" >
+          Inclui o texto:
+          <input
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
       </form>
     );
   }
