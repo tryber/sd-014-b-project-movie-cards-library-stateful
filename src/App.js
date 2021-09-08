f2bd1e2bcec71c691cb254656eb5a2e458605b8f
Matheus Laurindo <matheus104521@gmail.com>
2021-09-08 17:22:21 -0300

    Volta com todos os testes do searchBar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Header from './components/Header';
 import './App.css';
 import SearchBar from './components/SearchBar';
+import MovieList from './components/MovieList';
+import movies from './data';
+import AddMovie from './components/AddMovie';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <SearchBar />
+      <MovieList movies={ movies } />
+      <AddMovie />
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,32 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+
+    this.setState = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    // eslint-disable-next-line no-unused-vars
+    const { onClick } = this.props;
+
+    return (
+      <p>Opa, vamo add um filme ae!</p>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
