f5b5d2e4a479e5d85bad9212b31a902ce615ccc2
Mariana Ferreira <marianadesouzaferreira01@gmail.com>
2021-09-08 16:54:42 -0300

    atualizacao teste github

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,33 @@
-// implement AddMovie component
+/* import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { onClick } = this.props;
+
+    return (
+      <div>
+        <form data-testid="search-bar-form" />
+      </div>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie; */
