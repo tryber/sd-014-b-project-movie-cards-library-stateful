d991b96db78c62699b0e71b901bc50ece5347da1
perchespierri <perchespierri@gmail.com>
2021-09-09 16:31:07 -0300

    Requisito 6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import Header from './components/Header';
 import './App.css';
+import Header from './components/Header';
 import SearchBar from './components/SearchBar';
+import AddMovie from './components/AddMovie';
 
 export default function App() {
   return (
     <div className="App">
       <Header />
       <SearchBar />
+      <AddMovie />
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,25 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+export default class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { subtitle, title, imagePath,
+      storyline, rating, genre } = this.state;
+    return (
+      <div>
+        teste
+      </div>
+    );
+  }
+}
