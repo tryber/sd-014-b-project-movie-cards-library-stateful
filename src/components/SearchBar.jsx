dbabd1bb0496ddcba35c810e837d1d37107e71ef
Thales Lima <thales.bplima@gmail.com>
2021-09-08 21:36:34 -0300

    add req 3-4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,8 +7,25 @@ class SearchBar extends Component {
       onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
 
     return (
-      <form>
-        <h1>Test</h1>
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-input" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            type="text"
+            data-testid="text-input"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+        <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+        </label>
       </form>
     );
   }
