714e6753c6edccbc99c2e9c3a6da450b27f89c9c
anastacioneto <anastacio.a.c.n@gmail.com>
2021-09-09 17:30:13 -0300

    adding requirements one and two

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,35 @@
-// implement AddMovie component here
+// import React, { Component } from 'react';
+
+// class AddMovie extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       subtitle: '',
+//       title: '',
+//       imagePath: '',
+//       storyline: '',
+//       rating: 0,
+//       genre: 'action',
+//     };
+//     this.onClick = this.onClick.bind(this);
+//   }
+
+//   onClick() {
+
+//   }
+
+//   render() {
+//     const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+//     return (
+//       <form data-testid="add-movie-form">
+//         <label htmlFor="inputTitle">
+//           Título
+//           <input id="inputTitle" type="text" value={ title } />
+//         </label>
+//       </form>
+//     );
+//   }
+// }
+
+// export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,81 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+// import MovieList from './MovieList';
+
+class SearchBar extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: '',
+      selectedGenre: '',
+    };
+  }
+
+  onSearchTextChange = (event) => {
+    this.setState({
+      searchText: event.target.value,
+    });
+  }
+
+  onBookmarkedChange = (event) => {
+    this.setState({
+      bookmarkedOnly: event.target.value,
+    });
+  }
+
+  onSelectedGenreChange = (event) => {
+    this.setState({
+      selectedGenre: event.target.value,
+    });
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    return (
+      <form className="formSearchBar" data-testid="search-bar-form">
+        <label htmlFor="inputSearch" data-testid="text-input-label">
+          Inclui o texto:
+          <br />
+          <input
+            id="inputSearch"
+            name="textSearch"
+            type="text"
+            value={ searchText }
+            onChange={ this.onSearchTextChange }
+          />
+        </label>
+        <br />
+        <label htmlFor="inputCheck" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            id="inputCheck"
+            data-testid="checkbox-input"
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ this.onBookmarkedChange }
+          />
+        </label>
+        <br />
+        <label htmlFor="inputSelect" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            id="inputSelect"
+            data-testid="select-input"
+            value={ selectedGenre }
+            onChange={ this.onSelectedGenreChange }
+          >
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
