d9270f36131bdc8cc09b6027da9bfa3582b4a597
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-09-09 19:26:32 -0300

    Requesito #6.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,39 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  /* constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+    this.addMovies = this.addMovies.bind(this);
+  } */
+
+  /* addMovies({ target }) {
+  } */
+
+  render() {
+    const { addMovies } = this.props;
+    return (
+      <form>
+        <input
+          value={ addMovies }
+        />
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  addMovies: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
