a8dc81b7f4543070581070d2fbc25eb18a36f4e4
brenoMatias <matiasbreno19@gmail.com>
2021-09-09 11:29:05 -0300

    req 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,35 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+    constructor (props){
+        super(props);
+        this.state = { // define o estado inicial das propriedades
+            title: '',
+            subtitle: '',
+            imagePath: '',
+            storyline: '',
+            rating: 0,
+            genre: '',
+        };
+
+    }
+
+
+    render(){
+
+    }
+
+    return(
+
+    )
+}
+}
+
+export default AddMovie;
+
+//Typically, in React constructors are only used for two purposes:
+//Initializing local state by assigning an object to this.state.
+//Binding event handler methods to an instance
+// You should not call setState() in the constructor(). Instead,
+// if your component needs to use local state, assign the initial state to this.state directly in the constructor:
