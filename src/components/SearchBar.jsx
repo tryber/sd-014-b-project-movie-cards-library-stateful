a2500c87f99e11fc4c1d0ac5c099bdf65d28a4cc
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-09-09 16:22:56 -0300

    Adicionando skip aos testes e implementando requisito 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,14 @@
 // implement SearchBar component here
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedOnlyChange, selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default SearchBar;
