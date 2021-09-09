fc3aa71964a8161671f89da4351bc52015c1b638
brenoMatias <matiasbreno19@gmail.com>
2021-09-09 18:15:33 -0300

    Req 15 e 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,19 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import movies from '../data';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+  }
+ 
+}
