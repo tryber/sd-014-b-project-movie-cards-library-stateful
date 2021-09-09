c0ce68e0f84853512d818ac2de54592218797caf
Cecilia Cacais <ceci.cacais@gmail.com>
2021-09-09 15:10:52 -0300

    Adiciona avaliação e genero em AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
   constructor() {
@@ -21,6 +21,7 @@ class AddMovie extends React.Component {
     console.log(event.target.value);
   }
 
+  // eslint-disable-next-line max-lines-per-function
   render() {
     return (
       <form data-testid="add-movie-form">
@@ -62,7 +63,29 @@ class AddMovie extends React.Component {
             onChange={ this.onClick }
           />
         </label>
-
+        <label htmlFor="avaliaNovo" data-testid="rating-input-label">
+          Avaliação
+          <input
+            id="avaliaNovo"
+            type="number"
+            value={ this.state.rating }
+            data-testid="rating-input"
+            onChange={ this.onClick }
+          />
+        </label>
+        <label htmlFor="generoNovo" data-testid="genre-input-label">
+          Gênero
+          <select
+            id="generoNovo"
+            value={ this.state.genre }
+            data-testid="genre-input"
+            onChange={ this.onClick }
+          >
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="Suspense" data-testid="genre-option">thriller</option>
+          </select>
+        </label>
       </form>
     );
   }
