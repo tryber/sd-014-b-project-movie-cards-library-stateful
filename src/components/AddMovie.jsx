c1596d5b5776f25f42badb510836d222e6e9dfda
Elithon Silva <elithonsilva@gmail.com>
2021-09-09 18:04:34 -0300

    corrige problema do onChange na implementação dos elementos do componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -17,14 +17,9 @@ class AddMovie extends React.Component {
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(event) {
+  handleChange({ target }) {
     this.setState({
-      subtitle: event.target.value,
-      title: event.target.value,
-      imagePath: event.target.value,
-      storyline: event.target.value,
-      rating: event.target.value,
-      genre: event.target.value,
+      [target.name]: target.value,
     });
   }
 
@@ -33,32 +28,33 @@ class AddMovie extends React.Component {
       <form data-testid="add-movie-form">
         <label htmlFor="title-input" data-testid="title-input-label">
           Título
-          <input type="text" value={ this.state.title } data-testid="title-input" onChange={ this.handleChange } />
+          <input name="title" type="text" value={ this.state.title } data-testid="title-input" onChange={ this.handleChange } />
         </label>
         <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
           Subtítulo
-          <input type="text" value={ this.state.subtitle } data-testid="subtitle-input" onChange={ this.handleChange } />
+          <input name="subtitle" type="text" value={ this.state.subtitle } data-testid="subtitle-input" onChange={ this.handleChange } />
         </label>
         <label htmlFor="image-input" data-testid="image-input-label">
           Imagem
-          <input type="text" value={ this.state.imagePath } data-testid="image-input" onChange={ this.handleChange } />
+          <input name="imagePath" type="text" value={ this.state.imagePath } data-testid="image-input" onChange={ this.handleChange } />
         </label>
         <label htmlFor="storyline-input" data-testid="storyline-input-label">
           Sinopse
-          <textarea value={ this.state.storyline } data-testid="storyline-input" onChange={ this.handleChange } />
+          <textarea name="storyline" value={ this.state.storyline } data-testid="storyline-input" onChange={ this.handleChange } />
         </label>
         <label htmlFor="rating-input" data-testid="rating-input-label">
           Avaliação
-          <input type="number" value={ this.state.rating } data-testid="rating-input" onChange={ this.handleChange } />
+          <input name="rating" type="number" value={ this.state.rating } data-testid="rating-input" onChange={ this.handleChange } />
         </label>
         <label htmlFor="genre-input" data-testid="genre-input-label">
           Gênero
-          <select value={ this.state.genre } data-testid="genre-input" onChange={ this.handleChange }>
+          <select name="genre" value={ this.state.genre } data-testid="genre-input" onChange={ this.handleChange }>
             <option value="action" data-testid="genre-option">Ação</option>
             <option value="comedy" data-testid="genre-option">Comédia</option>
             <option value="thriller" data-testid="genre-option">Suspense</option>
           </select>
         </label>
+        <button type="button" data-testid="send-button">Adicionar filme</button>
       </form>
     );
   }
