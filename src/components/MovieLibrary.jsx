800dac97d71b8ff99977f26b3886ba7af4ae6e82
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-09-09 19:26:38 +0100

    PropTypes de movies

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
@@ -53,5 +54,8 @@ class MovieLibrary extends Component {
     );
   }
 }
+MovieLibrary.propTypes = {
+  movies: PropTypes.func.isRequired,
+};
 
 export default MovieLibrary;
