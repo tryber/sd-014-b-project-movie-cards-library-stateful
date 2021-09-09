765610d9f991723e14ec83947892eb33c46de2dd
brenoMatias <matiasbreno19@gmail.com>
2021-09-09 11:05:27 -0300

    req 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -19,10 +19,21 @@ class SearchBar extends Component { // req1. passar as propriedades para o compo
           <input
             data-testid="text-input"
             type="text"
-            value={ searchText }
-            onChange={ onSearchTextChange }
+            value={ searchText } // A propriedade value do input deve receber o valor da prop searchText
+            onChange={ onSearchTextChange } // A propriedade onChange do input deve receber o valor da prop onSearchTextChange
           />
         </label>
+
+        <label htmlFor="checkbox" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            data-testid="checkbox-input"
+            type="checkbox"
+            checked={ bookmarkedOnly } // A propriedade checked do input deve receber o valor da prop bookmarkedOnly
+            onChange={ onBookmarkedChange } // A propriedade onChange do input deve receber o valor da prop onBookmarkedChange
+          />
+        </label>
+
       </form>
     );
   }
