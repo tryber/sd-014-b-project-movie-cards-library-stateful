26f9fdb124099a0dd7e224fd2cc5960676b9335b
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2021-09-08 21:00:36 -0300

    Requisito 18 corrigido

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import movies from './data';
 import Header from './components/Header';
 import './App.css';
+
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
-      <Header begining="" />
+      <Header />
+      <MovieLibrary movies={ movies } />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import '../App.css';
 import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
   constructor(props) {
@@ -10,30 +12,54 @@ class MovieLibrary extends Component {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies: [],
+      movies: props.movies,
+      standardMovieList: props.movies,
     };
+    this.changer = this.changer.bind(this);
+    this.tripleFilter = this.tripleFilter.bind(this);
   }
 
-  changer(keyName, { target }) {
+  tripleFilter() {
+    const { standardMovieList, bookmarkedOnly, selectedGenre, searchText } = this.state;
+    let output = standardMovieList;
+    output = bookmarkedOnly
+      ? output.filter(({ bookmarked }) => bookmarked) : output;
+    output = selectedGenre !== ''
+      ? output.filter(({ genre }) => genre === selectedGenre) : output;
+    output = searchText !== ''
+      ? output.filter(({ title, subtitle, storyline }) => title
+        .toUpperCase().includes(searchText.toUpperCase()) || subtitle
+        .toUpperCase().includes(searchText.toUpperCase()) || storyline
+        .toUpperCase().includes(searchText.toUpperCase())) : output;
+    return output;
+  }
+
+  changer(keyName, { target: { value } }) {
     this.setState({
-      [keyName]: target.value,
-    });
+      [keyName]: value }, () => this.setState({
+      movies: this.tripleFilter(),
+    }));
   }
 
   render() {
-    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    const {
+      searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
 
     return (
       <div>
-        { movies }
         <SearchBar
           searchText={ searchText }
-          onSearchTextChange={ (event) => this.changer('searchText', event) }
-          bookmarkedOnly={ bookmarkedOnly }
-          onBookmarkedChange={ (event) => this.changer('bookmarkedOnly', event) }
           selectedGenre={ selectedGenre }
-          onSelectedGenreChange={ (event) => this.changer('selectedGenre', event) }
+          bookmarkedOnly={ bookmarkedOnly }
+          onSearchTextChange={ (event) => this
+            .changer('searchText', event) }
+          onSelectedGenreChange={ (event) => this
+            .changer('selectedGenre', event) }
+          onBookmarkedChange={ (event) => this
+            .changer('bookmarkedOnly', event) }
         />
+        <MovieList movies={ movies } />
+        <AddMovie />
       </div>
     );
   }
