3dad09ebbe0d4135f6cbb423d7165765ad31daae
perchespierri <perchespierri@gmail.com>
2021-09-09 16:56:55 -0300

    Requisito 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -11,15 +11,47 @@ export default class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  } // Explicação no fim da página
 
   render() {
     const { subtitle, title, imagePath,
       storyline, rating, genre } = this.state;
     return (
       <div>
-        <form data-testid="add-movie-form" />
+        <form data-testid="add-movie-form">
+          <label htmlFor data-testid="title-input-label">
+            Título
+            <input
+              data-testid="title-input"
+              type="text"
+              value={ title }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor data-testid="subtitle-input-label">
+            Subtítulo
+            <input
+              type="text"
+              data-testid="title-input"
+              value={ subtitle }
+              onChange={ this.handleChange }
+            />
+          </label>
+        </form>
       </div>
     );
   }
 }
+
+// Em passos:
+// 1 - Recebe o event e desestrutura para pegar só event.target, que agora é target.
+// 2 - Do target pega target.name e target.value, desestruturando para name e value
+// 3 - Em setState, desestrutura o item que tiver chave name, mudando o valor pra value
