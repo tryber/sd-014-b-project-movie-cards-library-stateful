832fcb8d09c7832c56f54da4ffdecacb175ed8fb
Nataniel Santos <nathan.santos159@hotmail.com>
2021-09-09 15:30:36 -0300

    Requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -7,6 +7,13 @@ import AddMovie from './AddMovie';
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      searchText: "",
+      bookmarkedOnly: false,
+      selectedGenre: "",
+      movies: props.movies,
+    };
   }
 
   render() {
