d53b9983d6da28041149bda781f821507485fbac
icaroemanuel <icaro.emanuel@outlook.com>
2021-09-09 17:42:18 -0300

    Refatorei  a função render do AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,5 +1,11 @@
 // implement AddMovie component here
 import React from 'react';
+import InputTitle from './inputs-components/InputTitle';
+import InputSubtitle from './inputs-components/InputSubtitle';
+import InputImage from './inputs-components/InputImage';
+import InputTextArea from './inputs-components/InputTextArea';
+import InputNumber from './inputs-components/InputNumber';
+import InputSelect from './inputs-components/InputSelect';
 
 class AddMovie extends React.Component {
   constructor() {
@@ -29,69 +35,12 @@ class AddMovie extends React.Component {
     return (
       <section>
         <form data-testid="add-movie-form">
-          <label htmlFor="title-input" data-testid="title-input-label">
-            Título
-            <input
-              type="text"
-              name="title"
-              value={ title }
-              onChange={ this.addMovie }
-              data-testid="title-input"
-              id="title-input"
-            />
-          </label>
-          <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
-            Subtítulo
-            <input
-              type="text"
-              name="subtitle"
-              value={ subtitle }
-              onChange={ this.addMovie }
-              data-testid="subtitle-input"
-              id="subtitle-input"
-            />
-          </label>
-          <label htmlFor="image-input" data-testid="image-input-label">
-            Imagem
-            <input
-              type="text"
-              name="imagePath"
-              value={ imagePath }
-              onChange={ this.addMovie }
-              data-testid="image-input"
-              id="image-input"
-            />
-          </label>
-          <label htmlFor="storyline-input" data-testid="storyline-input-label">
-            Sinopse
-            <textarea
-              name="storyline" 
-              value={ storyline } 
-              onChange={ this.addMovie }
-              data-testid="storyline-input"
-              id="storyline-input"
-            />
-          </label>
-          <label htmlFor="rating-input" data-testid="rating-input-label">
-            Avaliação
-            <input
-              type="number"
-              name="rating" 
-              value={ rating } 
-              onChange={ this.addMovie }
-              data-testid="rating-input"
-              id="rating-input"
-            />
-          </label>
-          <label htmlFor="genre-input" data-testid="genre-input-label">
-            Gênero
-            <select name="genre" id="genre-input" value={ genre } data-testid="genre-input" onChange={this.addMovie}>
-              <option value="action" data-testid="genre-option">Ação</option>
-              <option value="comedy" data-testid="genre-option">Comédia</option>
-              <option value="thriller" data-testid="genre-option">Suspense</option>
-            </select>
-          </label>
-          <button type="submit" data-testid="send-button">Adicionar filme</button>
+          <InputTitle value={ title } onChange={ this.addMovie } />
+          <InputSubtitle value={ subtitle } onChange={ this.addMovie } />
+          <InputImage value={ imagePath } onChange={ this.addMovie } />
+          <InputTextArea value={ storyline } onChange={ this.addMovie } />
+          <InputNumber value={ rating } onChange={ this.addMovie } />
+          <InputSelect value={ genre } onChange={ this.addMovie } />
         </form>
       </section>
     );
