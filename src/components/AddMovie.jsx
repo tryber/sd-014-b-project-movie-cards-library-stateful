ee91fa207ae6c77d31330d701d37fa3144537431
Daniel <daniel.acoelho@outlook.com>
2021-09-09 17:07:15 -0300

    varias correçoes

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,57 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            subtitle: ' ',
+            title: ' ',
+            imagePath: ' ',
+            storyLine: ' ',
+            rating: 0,
+            genre: 'action',
+        };
+    }
+
+    render() {
+        return (
+          <form data-testid="add-movie-form">
+            <label htmlFor="title">
+              Título:
+              <input type="text" name="title" id="title" value="" />
+            </label>
+            <label htmlFor="subtitle">
+              Subtítulo:
+              <input type="text" name="subtitle" id="subtitle" value="" />
+            </label>
+            <label htmlFor="imagePath">
+              Capa:
+              <input type="text" name="imagePath" id="imagePath" value="" />
+            </label>
+            <label htmlFor="rating">
+              Nota:
+              <input type="number" name="rating" id="rating" value="" />
+            </label>
+            <label htmlFor="storyline">
+              Sinopse:
+              <textarea name="storyline" id="storyline" value="" />
+            </label>
+            <label htmlFor="genre">
+              Gênero:
+              <select
+                value=""
+                name="genre"
+                id="genre"
+              >
+                <option value="">Todos</option>
+                <option value="action">Ação</option>
+                <option value="comedy">Comédia</option>
+                <option value="thriller">Suspense</option>
+              </select>
+            </label>
+          </form>
+        );
+      }
+    }
+    
+    export default AddMovie;
