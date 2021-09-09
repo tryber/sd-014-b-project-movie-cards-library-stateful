13a4a5b66c717c7d45024f7f3ae86ae8ea0a7724
Diego Costa <dgrcosta@gmail.com>
2021-09-09 09:17:00 -0300

    Requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -32,8 +32,19 @@ class SearchBar extends Component {
             data-testid="checkbox-input"
           />
         </label>
-        {selectedGenre}
-        {onSelectedGenreChange}
+        <label htmlFor="select-input" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            data-testid="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
