568c294b547907ad3533017f39cc599f665ce2e8
andremorsch <andremorsch37@gmail.com>
2021-09-09 15:45:35 -0300

    requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,16 @@
-// implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar'
+
+class MovieLibrary extends React.Component {
+  render() {
+    // const { movies } = this.props;
+
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
