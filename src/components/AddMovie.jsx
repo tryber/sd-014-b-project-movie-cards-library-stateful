0ee28dccb69b1acc7d5704e73fe559e37ce836ab
Arturo Vaine <arturo.vaine@gmail.com>
2021-09-09 13:45:45 -0300

    Update AddMovie e MovieLibrary

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,59 @@
-// implement AddMovie component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      /* subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action', */
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+  /*      handleChange(event) {
+        this.setState({
+            subtitle: '',
+            title: '',
+            imagePath: '',
+            storyline: '',
+            rating: 0,
+            genre: 'action',
+          email: event.target.value,
+        });
+      }
+  */
+
+  render() {
+    return (
+      <div>
+        <p>teste</p>
+      </div>
+    );
+  }
+}
+
+/*
+subtitle: guarda o subtítulo preenchido no formulário por quem usa a aplicação;
+title: guarda o título preenchido no formulário por quem usa a aplicação;
+imagePath: guarda o caminho da imagem preenchido no formulário por quem usa a aplicação;
+storyline: guarda a sinopse do filme escrita no formulário por quem usa a aplicação;
+rating: guarda a nota de avaliação dada no formulário por quem usa a aplicação;
+genre: guarda o gênero do filme selecionado no formulário por quem usa a aplicação.
+
+*/
+/*
+AddMovie.PropTypes = {
+  subtitles: PropTypes.string,
+  title: PropTypes.string,
+  imagePath: PropTypes.string,
+  storyline: PropTypes.string,
+  rating: PropTypes.number,
+  genre: PropTypes.string,
+};
+*/
+export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,25 +1,29 @@
-// implement MovieLibrary component here
-/* import React, { Component } from 'react';
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
 
-import MovieList from './MovieList';
+// import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
+// import { movies } from '../data';
 
 class MovieLibrary extends Component {
-  constructor(props) {
-    super(props);
-  }
+//  constructor(props) {
+//    super(props);
+//  }
 
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={this.props.movies} />
+        {/* <MovieList movies={this.props.movies} /> */}
         <AddMovie />
       </div>
     );
   }
 }
-
-export default MovieLibrary; */
+/*
+MovieLibrary.PropTypes = {
+};
+*/
+export default MovieLibrary;
