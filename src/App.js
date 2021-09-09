15fdb0f9380ac2669718a2b16359d1331c39b724
Thyara <thyaranonato@outlook.com>
2021-09-09 20:34:34 -0300

    Adiciona requisitos 16 ao 19

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
-import MovieList from './components/MovieList';
 import movies from './data';
-import SearchBar from './components/SearchBar';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar />
-      <MovieList movies={ movies } />
+      <MovieLibrary movies={ movies } />
     </div>
   );
 }
