7f29638c66de797a8d4eb2e9fec2bf166263a922
BrennoBener <brennobbener@live.com>
2021-09-09 15:30:58 -0300

    requisitos 3 e 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -13,7 +13,26 @@ export default class SearchBar extends Component {
 
     return (
       <form data-testid="search-bar-form">
-
+        <label data-testid="text-input-label" htmlFor="search-text">
+          <p>Inclui o texto:</p>
+          <input
+            data-testid="text-input"
+            id="search-text"
+            type="text"
+            onChange={ onSearchTextChange }
+            value={ searchText }
+          />
+        </label>
+        <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
+          <p> Mostrar somente favoritos </p>
+          <input
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+            id="checkbox-input"
+          />
+        </label>
       </form>
     );
   }
