8745b39bebe413564a78aca2508acf6140721571
enzo <enzo@pop-os.localdomain>
2021-09-09 12:30:45 -0300

    Requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,26 +1,16 @@
 // implement MovieLibrary component here
-// import React from 'react';
-// import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// class MovieLibrary extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//         searchText:'',
-//         bookmarkedOnly: false,
-//         selectedGenre: '',
-//         movies: '',
-//     };
-//   }
+class MovieLibrary extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return <p> Renderizou </p>;
+  }
+}
 
-//   render() {
-//     const { movies } = this.props;
-//     return <p> Renderizou </p>;
-//   }
-// }
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf.isRequired,
+};
 
-// MovieLibrary.propTypes = {
-//   movies: PropTypes.arrayOf.isRequired,
-// };
-
-// export default MovieLibrary;
+export default MovieLibrary;
