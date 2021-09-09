985dd0e4cdb7c579bce8e5625a2f0b3c7281c8e4
icaroemanuel <icaro.emanuel@outlook.com>
2021-09-09 17:24:57 -0300

    Criação do component InputSelect

---
+++ src/components/inputs-components/InputSelect.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class InputSelect extends React.Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <label htmlFor="select-input" data-testid="genre-input-label">
+        <p>Gênero</p>
+        <select
+          name="genre"
+          id="select-input"
+          value={ value }
+          data-testid="genre-input"
+          onChange={ onChange }
+        >
+          <option value="action" data-testid="genre-option">Ação</option>
+          <option value="comedy" data-testid="genre-option">Comédia</option>
+          <option value="thriller" data-testid="genre-option">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+}
+
+InputSelect.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default InputSelect;
