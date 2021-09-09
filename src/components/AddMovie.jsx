37787470f2afcf4ec9eb9adcf5905e8c693ac958
brenoMatias <matiasbreno19@gmail.com>
2021-09-09 12:17:03 -0300

    req 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,35 +1,44 @@
 // implement AddMovie component here
 import React, { Component } from 'react';
+import AddTitle from './AddMovieComponents/AddTitle';
 
 class AddMovie extends Component {
-    constructor (props){
-        super(props);
-        this.state = { // define o estado inicial das propriedades
-            title: '',
-            subtitle: '',
-            imagePath: '',
-            storyline: '',
-            rating: 0,
-            genre: '',
-        };
+  constructor(props) {
+    super(props);
+    this.state = { // define o estado inicial das propriedades
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: '',
+    };
+  }
+
+    handleChange = (event) => { // seleciona os eventos de input
+      const { name, value } = event.target;
+      this.setState({ // update the state key corresponding to the given input name:
+        [name]: value, // setState() automatically merges a partial state into the current state
+      });
+    } // handleChange vai definir o evento alvo e o que fazer com aquele resultado recebido
+
+    render() {
+      const { title, subtitle, imagePath, storyline, rating, genre } = this.state; // renderiza os estados inicias
+      return (
+        <form data-testid="add-movie-form">
+          <AddTitle handleChange={ this.handleChange } value={ title } />
+        </form>
+      );
     }
-
-
-    render(){
-     <form data-testid="add-movie-form">
-         a
-     </form>
-    }
-
-
-   return()
-}
 }
 
 export default AddMovie;
 
-//Typically, in React constructors are only used for two purposes:
-//Initializing local state by assigning an object to this.state.
-//Binding event handler methods to an instance
+// Typically, in React constructors are only used for two purposes:
+// Initializing local state by assigning an object to this.state.
+// Binding event handler methods to an instance
 // You should not call setState() in the constructor(). Instead,
 // if your component needs to use local state, assign the initial state to this.state directly in the constructor:
+// handleChange:
+// When you need to handle multiple controlled input elements, you can add a name attribute to each element and let
+// the handler function choose what to do based on the value of event.target.name.
---
+++ src/components/AddMovieComponents/AddTitle.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class AddTitle extends React.Component {
+  render() {
+    const { handleChange, value } = this.props;
+    return (
+      <label htmlFor="add-title" data-testid="title-input-label">
+        Título:
+        <input
+          name="add-title"
+          type="text"
+          id="text"
+          data-testid="title-input"
+          value={ value }
+          onChange={ handleChange } // The onchange event occurs when the value of an element has been changed.
+        />
+      </label>
+    );
+  }
+}
+
+export default AddTitle;
