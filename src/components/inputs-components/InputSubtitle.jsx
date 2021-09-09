4cf30aedb6cced3363381235bcdda043259d294b
icaroemanuel <icaro.emanuel@outlook.com>
2021-09-09 17:04:39 -0300

    Criação do component InputSubtitle

---
+++ src/components/inputs-components/InputSubtitle.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class InputSubtitle extends React.Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+        <p>Subtítulo</p>
+        <input
+          name="subtitle"
+          type="text"
+          id="subtitle-input"
+          value={ value }
+          onChange={ onChange }
+          data-testid="subtitle-input"
+        />
+      </label>
+    );
+  }
+}
+
+InputSubtitle.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default InputSubtitle;
