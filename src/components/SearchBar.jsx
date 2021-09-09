bf4746d499c5fb532478765a9ee3ce0a76942d04
dsomarcelo <celoygm@gmail.com>
2021-09-09 12:20:11 -0300

    Commit Inicial

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,42 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+// import MovieList from './MovieList';
+
+class SearchBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchText: '',
+      // bookmarkedOnly = false,
+      // selectedGenre = '',
+      // onSelectedGenreChange(),
+    };
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+  }
+
+  onSearchTextChange(event) {
+    console.log(this);
+    this.setState({ searchText: event.target.value });
+  }
+  // onBookmarkedChange(),
+
+  render() {
+    const { searchText } = this.state;
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="searchText" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            value={ searchText }
+            onChange={ this.onSearchTextChange }
+            type="text"
+            name="searchText"
+            id="searchText"
+            data-testid="text-input"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
