dde75a25ad02421acf9eb89b77be0ef62ac2b552
andremorsch <andremorsch37@gmail.com>
2021-09-09 16:17:49 -0300

    requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
 import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
-class MovieLibrary extends React.Component {
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // searchText: '',
+      // bookmarkedOnly: false,
+      // selectedGenre: '',
+      // movies: '',
+    };
+  }
+
   render() {
-    // const { movies } = this.props;
+    const { movies } = this.props;
 
     return (
       <div>
+        <h2> My awesome movie library </h2>
         <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieLibrary;
