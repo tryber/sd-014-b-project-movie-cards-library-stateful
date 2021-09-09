26c66e0945c8704efed81d4cacc4875376b7271b
Elithon Silva <elithonsilva@gmail.com>
2021-09-09 16:32:41 -0300

    Renderiza formulário e inputs de titulo e subtitulo

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,9 +2,55 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
-  render() {
+  constructor() {
+    super();
 
-    return ()
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      subtitle: event.target.value,
+      title: event.target.value,
+      imagePath: event.target.value,
+      storyline: event.target.value,
+      rating: event.target.value,
+      genre: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            value={ this.state.title }
+            data-testid="title-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            value={ this.state.subtitle }
+            data-testid="subtitle-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
+    );
   }
 }
 
