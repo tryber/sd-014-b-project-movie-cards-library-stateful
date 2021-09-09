bed374c5d4384999bf2fb6e3a519e124d1d8ced6
Davisralves <davialves324@gmail.com>
2021-09-09 11:32:52 -0300

    finalizando requisito 18

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -27,8 +27,30 @@ class MovieLibrary extends React.Component {
     } else { this.setState({ [name]: value }); }
   }
 
+  filtermoviesByText = (films, searchText) => films.filter(
+    (movie) => movie.title.toUpperCase().includes(searchText.toUpperCase())
+        || movie.subtitle.toUpperCase().includes(searchText.toUpperCase())
+        || movie.storyline.toUpperCase().includes(searchText.toUpperCase()),
+  );
+
+  filterMoviesByGenre = (films, selectedGenre) => {
+    if (selectedGenre !== '') {
+      return films.filter((movie) => movie.genre === selectedGenre);
+    }
+    return films;
+  }
+
+  filterMoviesByBookMarked = (films, bookmarkedOnly) => {
+    if (bookmarkedOnly) { return films.filter((movie) => movie.bookmarked); }
+    return films;
+  }
+
   render() {
     const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    let filteredMovies = this.filterMoviesByBookMarked(movies, bookmarkedOnly);
+    filteredMovies = this.filtermoviesByText(filteredMovies, searchText);
+    filteredMovies = this.filterMoviesByGenre(filteredMovies, selectedGenre);
+
     return (
       <main>
         <SearchBar
@@ -40,7 +62,7 @@ class MovieLibrary extends React.Component {
           onBookmarkedChange={ this.handleClick }
           onSelectedGenreChange={ this.handleClick }
         />
-        <MovieList movies={ movies } />
+        <MovieList movies={ filteredMovies } />
       </main>
     );
   }
