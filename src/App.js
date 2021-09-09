75ac16ecad1e928ddbf27d6fd4e0d54812d6776c
Rafael Moraes - Turma 14 - Tribo B <rafael1moraes1rocha@gmail.com>
2021-09-09 15:52:15 -0300

    Projeto Movie Cards Library Stateful :clapper: :movie_camera: - Requisito 17

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './components/Header';
-import SearchBar from './components/SearchBar';
 import AddMovie from './components/AddMovie';
 import MovieLibrary from './components/MovieLibrary';
 import './App.css';
