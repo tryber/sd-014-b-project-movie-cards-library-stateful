e9645a5fd0cb5c62c9ce81258c6c8d0447fc575b
Anderson Leite <andersonleite.dev@gmail.com>
2021-09-09 08:57:19 -0300

    Req 8 - Redering input text in form AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      title: '',
+    };
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { title } = this.state;
     return (
       <form data-testid="add-movie-form">
-        component
+        <label
+          htmlFor="title-input"
+          data-testid="title-input-label"
+        >
+          Título
+          <input
+            type="text"
+            id="title-input"
+            data-testid="title-input"
+            name="title"
+            value={ title }
+            onChange={ this.handleChange }
+          />
+        </label>
       </form>
     );
   }
