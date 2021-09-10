84cfb4c371de266d5d29822c4350a066fee0f0f2
dsomarcelo <celoygm@gmail.com>
2021-09-09 21:19:57 -0300

    req 6,7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,29 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     subtitle: '',
+  //     title: '',
+  //     imagePath: '',
+  //     storyline: '',
+  //     rating: 0,
+  //     genre: 'action'
+  //   };
+  // }
+
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="title">
+          Titulo
+          <input type="text" name="title" id="title" />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
