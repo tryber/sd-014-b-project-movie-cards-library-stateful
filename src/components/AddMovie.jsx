c84d752e88b18e85b4fccba6645932e690156af2
Adilson <adilsongb.rabelo@gmail.com>
2021-09-09 23:49:04 -0300

    Requisito 9 :heavy_check_mark:

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -6,17 +6,18 @@ class AddMovie extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.state = {
       title: '',
+      subtitle: '',
     };
   }
 
   handleChange({ target }) {
     this.setState({
-      title: target.value,
+      [target.name]: target.value,
     });
   }
 
   render() {
-    const { title } = this.state;
+    const { title, subtitle } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="titleFormAdd" data-testid="title-input-label">
@@ -24,8 +25,21 @@ class AddMovie extends Component {
           <input
             type="text"
             id="titleFormAdd"
+            name="title"
             value={ title }
             data-testid="title-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+
+        <label htmlFor="subtitleFormAdd" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            id="subtitleFormAdd"
+            name="subtitle"
+            value={ subtitle }
+            data-testid="subtitle-input"
             onChange={ this.handleChange }
           />
         </label>
