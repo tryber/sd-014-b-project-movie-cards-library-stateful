b40dc2c58607be6bb47cb10f0868b72b77e7270a
raphael-caninde <raphael.caninde@gmail.com>
2021-09-10 00:57:31 -0300

    Requisitos 1-4: criado o form, contendo o input e o checkbox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import MovieList from './components/MovieList';
+import movies from './data';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
+      <MovieList movies= { movies } />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,41 @@
 // implement SearchBar component here
+import React, { Component } from 'react'
+
+export default class SearchBar extends Component {
+
+  render() {
+    const { 
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange
+    } = this.props;
+
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="" >
+          Inclui o texto:
+          <input
+          name="searchText"
+          value={ searchText }
+          data-testid="text-input"
+          onChange={ onSearchTextChange }
+          type="text"
+          />
+        </label>
+        <label data-testid="checkbox-input-label" htmlFor="">
+          Mostrar somente favoritos
+          <input
+          name="bookmarkedOnly"
+          type="checkbox"
+          checked={ bookmarkedOnly }
+          onChange= { onBookmarkedChange }
+          data-testid="checkbox-input"
+          />
+        </label>
+      </form>
+    )
+  }
+}
