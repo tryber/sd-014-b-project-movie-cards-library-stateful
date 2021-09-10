566a5dc37d10b04cc527413cbd507dfac227ab52
Cassio Mascarenhas <csmlvx@gmail.com>
2021-09-10 07:22:42 -0300

    Requisito 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,11 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  render(){
+      return <form data-testid="add-movie-form">
+          a
+      </form>;
+  }
+}
+
+export default AddMovie;
