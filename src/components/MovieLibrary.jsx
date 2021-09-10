7aabcc67a1a15298f0e4c003bf4dfe8d7aeb88ec
Cecilia Cacais <ceci.cacais@gmail.com>
2021-09-10 20:04:14 -0300

    Arrumando o requisito 12

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,35 +5,16 @@ import AddMovie from './AddMovie';
 import movies from '../data';
 
 class MovieLibrary extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      searchText: '',
-      bookmarkedOnly: false,
-      selectedGenre: '',
-    };
-  }
-
   render() {
-    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar
-          searchText={ searchText }
-          bookmarkedOnly={ bookmarkedOnly }
-          selectedGenre={ selectedGenre }
-        />
+        <SearchBar />
         <MovieList movies={ movies } />
         <AddMovie />
       </div>
     );
   }
 }
-
-// MovieLibrary.propTypes = {
-//     movies: PropTypes.arrayOf(SearchBar).isRequired,
-// }
 
 export default MovieLibrary;
