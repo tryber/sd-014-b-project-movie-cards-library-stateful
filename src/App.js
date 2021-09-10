95b0025ec3b66a9f28e1b55259f921eb1dc7c915
Paulo França <paulovinicius.franca@gmail.com>
2021-09-10 11:42:15 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
+// import SearchBar from './components/SearchBar';
 import './App.css';
+import MovieLibrary from './components/MovieLibrary';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <MovieLibrary />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
 import MovieList from './MovieList';
-import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList movies={ this.props.movies } />
+        <MovieList />
         <AddMovie />
       </div>
     );
   }
 }
+
+// MovieLibrary.propTypes = {
+//   movies: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       title: PropTypes.string.isRequired,
+//       subtitle: PropTypes.string.isRequired,
+//       storyline: PropTypes.string.isRequired,
+//       rating: PropTypes.number.isRequired,
+//       imagePath: PropTypes.string.isRequired,
+//     }),
+//   ).isRequired,
+// };
 
 export default MovieLibrary;
