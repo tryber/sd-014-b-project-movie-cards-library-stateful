96ea7b84b5e07ae373cbcb67ad9144a506a91a6c
Victoria Lorena <victorialorenaf.gomes@gmail.com>
2021-09-10 09:52:25 -0300

    item 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -28,11 +28,20 @@ class SearchBar extends Component {
             onChange={ onBookmarkedChange }
           />
         </label>
-        <div>
-          { selectedGenre }
-          onSelectedGenreChange=
-          { onSelectedGenreChange }
-        </div>
+        <label htmlFor="genero" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            id="genero"
+            data-testid="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
