eb06a6c2ce18616cc88f5c130db7882855dfff40
Elithon Silva <elithonsilva@gmail.com>
2021-09-10 13:03:15 -0300

    define estado inicial de MovieLibrary e renderiza o componente SearchBar dentro dele

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,9 +1,25 @@
 // implement MovieLibrary component here
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movie: null,
+    };
+  }
+
   render() {
-    return ()
+    return (
+      <section>
+        <SearchBar />
+      </section>
+    );
   }
 }
 
