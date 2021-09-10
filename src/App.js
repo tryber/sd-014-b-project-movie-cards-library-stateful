6411dd886667a316c9e4edafae2eb0dbaecf91fa
BrennoBener <brennobbener@live.com>
2021-09-10 10:53:45 -0300

    corrigindo erro do app e lint

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
-import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
 import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      < MovieLibrary movies={ movies } />
+      <MovieLibrary movies={ movies } />
     </div>
   );
 }
