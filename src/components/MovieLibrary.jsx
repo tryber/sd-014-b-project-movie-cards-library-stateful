bbe16ad23694b36f1bbc874fd4cc67ed0a0503ff
Anderson Leite <andersonleite.dev@gmail.com>
2021-09-10 11:28:32 -0300

    Req 15 - Component MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,13 @@
-// implement MovieLibrary component here
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <div>
+        component
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
