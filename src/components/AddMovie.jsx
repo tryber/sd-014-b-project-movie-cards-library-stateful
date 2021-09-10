9f7ad2b46959dacf57fb8b63b196a743abafe564
BrennoBener <brennobbener@live.com>
2021-09-10 09:17:39 -0300

    requisitos 7 ao 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -11,13 +11,43 @@ export default class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
+    this.changeInput = this.changeInput.bind(this);
+  }
+
+  changeInput({ target }) {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
   }
 
   render() {
     const { onClick } = this.props;
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-
+        <label htmlFor="input-title" data-testid="title-input-label">
+          <p>Título</p>
+          <input
+            type="text"
+            data-testid="title-input"
+            id="input-text"
+            name="title"
+            value={ title }
+            onChange={ this.changeInput }
+          />
+        </label>
+        <label htmlFor="input-subtitle" data-testid="subtitle-input-label">
+          <p>Subtítulo</p>
+          <input
+            type="text"
+            data-testid="subtitle-input"
+            id="input-subtitle"
+            name="subtitle"
+            value={ subtitle }
+            onChange={ this.changeInput }
+          />
+        </label>
       </form>
     );
   }
