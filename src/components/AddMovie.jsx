ee409b4e3ce7cbec482634866218e2e58583120f
Victoria Lorena <victorialorenaf.gomes@gmail.com>
2021-09-10 10:49:16 -0300

    Item 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,48 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const inicialState = {
+  subtitle: '',
+  title: '',
+  imagePath: '',
+  storyline: '',
+  rating: 0,
+  genre: 'action',
+};
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = inicialState;
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    const { onClick } = this.props;
+    return (
+      <div>
+        subtitle=
+        { subtitle }
+        title=
+        { title }
+        imagePath=
+        { imagePath }
+        storyline=
+        { storyline }
+        rating=
+        { rating }
+        genre=
+        { genre }
+        onClick=
+        { onClick }
+      </div>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
