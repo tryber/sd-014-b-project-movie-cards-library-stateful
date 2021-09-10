559ac93906a01e8df749909c62b5310733b11dea
dsomarcelo <celoygm@gmail.com>
2021-09-09 21:38:34 -0300

    req 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,24 +2,36 @@ import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 
 class AddMovie extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     subtitle: '',
-  //     title: '',
-  //     imagePath: '',
-  //     storyline: '',
-  //     rating: 0,
-  //     genre: 'action'
-  //   };
-  // }
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      // subtitle: '',
+      //     imagePath: '',
+      //     storyline: '',
+      //     rating: 0,
+      //     genre: 'action'
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ title: event.target.value });
+  }
 
   render() {
+    const { title } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label htmlFor="title">
-          Titulo
-          <input type="text" name="title" id="title" />
+        <label htmlFor="title" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            name="title"
+            id="title"
+            data-testid="title-input"
+            value={ title }
+            onChange={ this.handleChange }
+          />
         </label>
       </form>
     );
