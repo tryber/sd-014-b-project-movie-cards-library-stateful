eef9fdd139bc728067841e0ce5c8bc6bd4ed49c9
Paulo França <paulovinicius.franca@gmail.com>
2021-09-10 15:30:46 -0300

    Requisitos 11, 12 e 13

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -5,12 +5,12 @@ class AddMovie extends React.Component {
   constructor() {
     super();
     this.state = {
-      subtitle: "",
-      title: "",
-      imagePath: "",
-      storyLine: "",
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
       rating: 0,
-      genre: "action",
+      genre: 'action',
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -59,6 +59,38 @@ class AddMovie extends React.Component {
             value={ this.state.imagePath }
             onChange={ this.handleChange }
           />
+        </label>
+        <label htmlFor="storyline" data-testid="storyline-input-label">
+          Sinopse
+          <textarea
+            name="storyline"
+            data-testid="storyline-input"
+            value={ this.state.storyline }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="rating" data-testid="rating-input-label">
+          Avaliação
+          <input
+            name="rating"
+            data-testid="rating-input"
+            type="number"
+            value={ this.state.rating }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="genre" data-testid="genre-input-label">
+          Gênero
+          <select
+            name="genre"
+            data-testid="genre-input"
+            value={ this.state.genre }
+            onChange={ this.handleChange }
+          >
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+          </select>
         </label>
       </div>
     );
