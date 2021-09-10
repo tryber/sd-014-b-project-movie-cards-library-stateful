d1c0e6bf42c316720a67e5012bddaf4aa9b705ce
Mathluiz23 <matheus_luizs@hotmail.com>
2021-09-09 22:51:54 -0300

    Requisito 14, 15, 16, 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,43 +1,52 @@
-// implement MovieLibrary component here
-// import React, { Component } from 'react';
+// // implement MovieLibrary component here
+import React, { Component } from 'react';
 // import movies from '../data';
 // import MovieList from './MovieList';
-// import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
-// class MovieLibrary extends Component {
-//   constructor(props) {
-//     super(props);
-//     console.log(this.props);
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props);
 
-//     this.state = {
-//       searchText: '',
-//       bookmarkedOnly: false,
-//       selectedGenre: '',
-//     };
-//   }
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+  }
 
-//   onSearchTextChange = (event) => {
-//     this.setState({
-//       searchText: event.target.value,
-//     });
-//   }
+  render() {
+    const {
+      searchText,
+      bookmarkedOnly,
+      selectedGenre,
+      movies,
+    } = this.state;
 
-//   onBookmarkedChange = (event) => {
-//     this.setState({
-//     bookmarkedOnly: event.target.checked,
-//   });
-// }
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.handleChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.handleChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.handleChange }
+        />
+        <AddMovie />
+      </div>
+    );
+  }
+}
 
-//   render() {
-//     return (
-//       <div>
-//         <h2> My awesome movie library </h2>
-//         <SearchBar />
-//         <MovieList movies={ movies } />
-//         <AddMovie />
-//       </div>
-//     );
-//   }
-// }
-// export default MovieLibrary;
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
+
+export default MovieLibrary;
