68248fa8b13f21b04e33eb6d1acc880844d64269
Emmanuel <EmmanuelRodriguesBoo@gmail.com>
2021-09-10 01:15:17 -0300

    Requisitos iniciais

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,53 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: '',
+      genre: ''
+    }
+  }
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label">
+          Título
+          <input data-testid="title-input" onChange={"titulo"} type="text" value={this.state.title}/>
+        </label>
+        <label data-testid="subtitle-input-label">
+          Subtítulo
+          <input data-testid="subtitle-input" onChange={"subtitulo"} type="text" value={this.state.title}/>
+        </label>
+        <label data-testid="image-input-label">
+          Imagem
+          <input data-testid="image-input" onChange={"imagePath"} type="text" />
+        </label>
+        <label data-testid="storyline-input-label">
+          Sinopse
+          <textarea data-testid="storyline-input" onChange="storyline" />
+        </label>
+        <label data-testid="rating-input-label">
+          Avaliação
+          <input data-testid="rating-input" onChange="rating" type="number" />
+        </label>
+        <label data-testid="genre-input-label">
+          Gênero
+          <select data-testid="genre-input" onChange={"genreValue"} value={"genre"}>
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
+        </label >
+          <input data-testid="send-button" type="button" onClick={"addmovie"} value="Adicionar filme" />
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,32 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label">
+          Inclui o texto:
+          <input data-testid="text-input" type="text" onChange={"onSearchTextChange"} value={"texto"} />
+        </label>
+        <label data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input data-testid="checkbox-input" type="checkbox" onChange={"onBookmarkedChange"} checked={"bookmarkedOnly"}/>
+        </label>
+        <label data-testid="select-input-label">
+          Filtrar por gênero
+          <select data-testid="select-input" onChange={"onSelectedGenreChange"} value={"selectedGenre"}>
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
