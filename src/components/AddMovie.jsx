3bba17f2cd45b2dd329fe6a9bef205b6a73f7a4f
dhenycruz <dheniarley.ds@gmail.com>
2021-09-10 00:42:41 -0300

    Requisito 6.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,23 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.setState = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    return (
+      <form />
+    );
+  }
+}
+
+export default AddMovie;
