02da81685bc0a3d087cb59027fa90c3cf180b102
Adilson <adilsongb.rabelo@gmail.com>
2021-09-10 00:05:34 -0300

    Requisito 11 :heavy_check_mark:

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -8,51 +8,54 @@ class AddMovie extends Component {
       title: '',
       subtitle: '',
       imagePath: '',
+      storyline: '',
     };
   }
 
   handleChange({ target }) {
-    this.setState({
-      [target.name]: target.value,
-    });
+    this.setState({ [target.name]: target.value });
   }
 
   render() {
-    const { title, subtitle, imagePath } = this.state;
+    const { title, subtitle, imagePath, storyline } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="titleFormAdd" data-testid="title-input-label">
           Título
           <input
             type="text"
-            id="titleFormAdd"
             name="title"
             value={ title }
             data-testid="title-input"
             onChange={ this.handleChange }
           />
         </label>
-
         <label htmlFor="subtitleFormAdd" data-testid="subtitle-input-label">
           Subtítulo
           <input
             type="text"
-            id="subtitleFormAdd"
             name="subtitle"
             value={ subtitle }
             data-testid="subtitle-input"
             onChange={ this.handleChange }
           />
         </label>
-
         <label htmlFor="imagePathFormAdd" data-testid="image-input-label">
           Imagem
           <input
             type="text"
-            id="imagePathFormAdd"
             name="imagePath"
             value={ imagePath }
             data-testid="image-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="storylineFormAdd" data-testid="storyline-input-label">
+          Sinopse
+          <textarea
+            value={ storyline }
+            name="storyline"
+            data-testid="storyline-input"
             onChange={ this.handleChange }
           />
         </label>
