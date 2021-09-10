6b72ca0a8f3cc4e226cc96349b9cd46f1b73013c
BrennoBener <brennobbener@live.com>
2021-09-10 10:03:11 -0300

    criar inputs para requisitos

---
+++ src/components/InputRating.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class InputRating extends Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <label htmlFor="select-input" data-testid="rating-input-label">
+        <p>Avaliação</p>
+        <input
+          id="rating-input"
+          name="rating"
+          value={ value }
+          onChange={ onChange }
+          type="number"
+          data-testid="rating-input"
+        />
+      </label>
+    );
+  }
+}
+
+InputRating.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
