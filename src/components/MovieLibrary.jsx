aaf1859a171f8990bdd9a630c353259b9b2b0e22
José Victor Macedo Veloso <vitovls37@gmail.com>
2021-09-11 13:23:12 -0300

    Requisito 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,11 +1,32 @@
 // implement MovieLibrary component here
 import React from 'react';
+import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+import listMovies from '../data';
 
 class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: [],
+    };
+  }
+
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
-      <AddMovie onClick={ () => {} } />);
+      <section>
+        <SearchBar />
+        <MovieList movies={ listMovies } />
+        <AddMovie onClick={ () => {} } />
+        <div>{ `${searchText} ${bookmarkedOnly} ${selectedGenre} ${movies}` }</div>
+      </section>
+    );
   }
 }
 
