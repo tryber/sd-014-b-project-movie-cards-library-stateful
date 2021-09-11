2bca8d71fe6c27ce0782fa32008fa49b072e8bfe
Ariane Ueti <arianeueti@gmail.com>
2021-09-11 19:18:18 -0300

    Requisito 10

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,67 +1,72 @@
 import React from 'react';
 
-const initialState = {
-  subtitle: '',
-  title: '',
-  imagePath: '',
-  storyline: '',
-  rating: 0,
-  genre: 'action',
-};
-
 class AddMovie extends React.Component {
   constructor() {
     super();
-    this.state = initialState;
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      // storyline: '',
+      // rating: 0,
+      // genre: 'action',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange = (event) => {
     this.setState({
       title: event.target.value,
       subtitle: event.target.value,
+      imagePath: event.target.value,
     });
-  };
+  }
 
   render() {
-    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
-
+    const { subtitle, title, imagePath } = this.state;
     return (
       <section>
         <form data-testid="add-movie-form">
-          <p>Olá!</p>
+          <label
+            htmlFor="title"
+            data-testid="title-input-label"
+          >
+            Título
+            <input
+              data-testid="title-input"
+              type="text"
+              value={ title }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label
+            htmlFor="subtitle"
+            data-testid="subtitle-input-label"
+          >
+            Subtítulo
+            <input
+              data-testid="subtitle-input"
+              type="text"
+              value={ subtitle }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label
+            htmlFor="image"
+            data-testid="image-input-label"
+          >
+            Imagem
+            <input
+              type="text"
+              alt={ title }
+              data-testid="image-input"
+              value={ imagePath }
+              onChange={ this.handleChange }
+            />
+          </label>
         </form>
-        <label
-          htmlFor="title"
-          data-testid="title-input-label"
-        >
-          Título
-          <input
-            data-testid="title-input"
-            type="text"
-            value={ title }
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label
-          htmlFor="subtitle"
-          data-testid="subtitle-input-label"
-        >
-          Subtítulo
-          <input
-            data-testid="subtitle-input"
-            type="text"
-            value={ subtitle }
-            onChange={ this.handleChange }
-          />
-        </label>
-        <div>
-          { imagePath }
-          { storyline }
-          { rating }
-          { genre }
-        </div>
       </section>
-
     );
   }
 }
