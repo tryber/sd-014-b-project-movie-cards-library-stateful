f4bcb686981634a05421e108267d3533f22c5990
killu4kun <mateeus.soarees@outlook.com>
2021-09-12 12:21:37 -0300

    Requisito 15 ao 18

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,58 @@
 // implement MovieLibrary component here gg
+import React, { Component } from 'react';
+import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+
+export default class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.handleMovie = this.handleMovie.bind(this);
+  }
+
+  handleMovie({ target }) {
+    console.log(target);
+  }
+
+  onSearchTextChange({ target }) {
+    this.setState({
+      searchText: target.value,
+    });
+  }
+
+  onBookmarkedChange({ target }) {
+    this.setState({
+      bookmarkedOnly: target.checked,
+    });
+  }
+
+  onSelectedGenreChange({ target }) {
+    this.setState({
+      selectedGenre: target.value,
+    });
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    return (
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+        <AddMovie onClick={ this.handleMovie } />
+      </div>
+    );
+  }
+}
