ffe98d17f86e86b71889578b50b7392179038a2d
larymanzo <lamosinha@hotmail.com>
2021-09-12 20:23:40 -0300

    Segundo requisito

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.onSearchTextChange = this.onSearchTextChange.bind;
+  // }
+
+  // onSearchTextChange() {
+  //   const { name } = target;
+  //   const value = target.type === 'checkbox' ? target.checked : target.value;
+
+  //   this.setState({
+  //     [name]: value,
+  //   });
+  // }
+
   render() {
     return (
-      <input />
+      <form data-testid="search-bar-form">
+        <label htmlFor="text">
+          Inclui o texto:
+          <input onChange={ this.onSearchTextChange } />
+          <input type="text" id="text" name="text" onChange={ this.onSearchTextChange } />
+        </label>
+      </form>
     );
   }
 }
