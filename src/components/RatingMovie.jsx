1d33ead6119e1539dd82d141496e3b416eb3b07c
jarana-2020 <julioaranabr@yahoo.com.br>
2021-09-12 13:43:35 -0300

    Altered RatingMovie

--- src/components/RatingMovie.jsx
+++ src/components/RatingMovie.jsx
@@ -5,16 +5,24 @@ class RatingMovie extends React.Component {
   render() {
     const { value, handleChange } = this.props;
     return (
-      <label data-testid="rating-input-label" htmlFor="rating-input">
-        Avaliação
+      <div className="mb-3">
+        <label
+          className="form-label"
+          data-testid="rating-input-label"
+          htmlFor="rating-input"
+        >
+          Avaliação
+        </label>
         <input
+          className="form-control"
           type="number"
           name="rating"
           value={ value }
           data-testid="rating-input"
           onChange={ handleChange }
         />
-      </label>
+      </div>
+
     );
   }
 }
