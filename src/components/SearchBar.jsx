23e1c55f40169546ba2431299488046eae61459d
vinicius-mns <vinicius_mns@hotmail.com>
2021-09-12 20:42:12 -0300

    requisito numero 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -13,19 +13,27 @@ class SearchBar extends React.Component {
 
     return (
       <form data-testid="search-bar-form">
+
         <label data-testid="text-input-label">
           Inclui o texto:
-
           <input
             data-testid="text-input"
+            type="text"
             value={ searchText }
             onChange={ onSearchTextChange }
           />
-
         </label>
 
-        {bookmarkedOnly}
-        {onBookmarkedChange}
+        <label data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            data-testid="checkbox-input"
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+        </label>
+
         {selectedGenre}
         {onSelectedGenreChange}
       </form>
