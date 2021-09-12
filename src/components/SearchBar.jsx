27b16899e585f1b220886cf52559979d289d684d
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-09-11 23:27:04 -0300

    requisito 1 iniciando projeto

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,21 @@
 // implement SearchBar component here
+import React, { Component } from "react";
+
+export default class SearchBar extends Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChang,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+    
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
