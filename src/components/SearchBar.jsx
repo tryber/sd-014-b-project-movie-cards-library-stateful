30718c65d65f31c1d17044a45257e46d804f2f8c
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-09-12 11:48:20 -0300

    requisitos 1 ao 5 finalizados

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,7 @@ export default class SearchBar extends Component {
   render() {
     const {
       searchText,
-      onSearchTextChang,
+      onSearchTextChange,
       bookmarkedOnly,
       onBookmarkedChange,
       selectedGenre,
@@ -15,7 +15,45 @@ export default class SearchBar extends Component {
     return (
       <div>
         <form data-testid="search-bar-form">
-          <label data-testid="text-input-label" >Inclui o texto:<input type="text" /></label>
+          <label data-testid="text-input-label">
+            Inclui o texto:
+            <input
+              type="text"
+              data-testid="text-input"
+              value={searchText}
+              onChange={onSearchTextChange}
+            />
+          </label>
+          <label data-testid="checkbox-input-label">
+            Mostrar somente favoritos
+            <input
+              type="checkbox"
+              checked={bookmarkedOnly}
+              onChange={onBookmarkedChange}
+              data-testid="checkbox-input"
+            />
+          </label>
+          <label data-testid="select-input-label">
+            Filtrar por gênero
+            <select
+              value={selectedGenre}
+              onChange={onSelectedGenreChange}
+              data-testid="select-input"
+            >
+              <option value="" data-testid="select-option">
+                Todos
+              </option>
+              <option value="action" data-testid="select-option">
+                Ação
+              </option>
+              <option value="comedy" data-testid="select-option">
+                Comédia
+              </option>
+              <option value="thriller" data-testid="select-option">
+                Suspense
+              </option>
+            </select>
+          </label>
         </form>
       </div>
     );
