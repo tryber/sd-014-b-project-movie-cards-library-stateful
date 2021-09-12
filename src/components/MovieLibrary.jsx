93297c293c5ab873a59643eb6798cc71646206e7
Tiago Ponce <tiagocponce@gmail.com>
2021-09-12 18:57:52 -0300

    Fix lint MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,4 +1,3 @@
-// implement MovieLibrary component here
 import React, { Component } from 'react';
 
 import MovieList from './MovieList';
@@ -6,18 +5,21 @@ import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
+    const { movies } = this.props;
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={this.props.movies} />
+        <MovieList movies={ movies } />
         <AddMovie />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.object,
+}.isRequired;
+
+export default MovieLibrary;
