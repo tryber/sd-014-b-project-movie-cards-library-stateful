5962b2b0acbee94d9c3f0095fd27585148b53f0b
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-09-12 23:52:34 -0300

    Requesito 13 corrigido.

--- src/components/Genre.jsx
+++ src/components/Genre.jsx
@@ -15,7 +15,7 @@ class Genre extends Component {
           value={ genreValue }
         >
           <option value="action" data-testid="genre-option">Ação</option>
-          <option value="comedy" data-testid="genre-option">Comedia</option>
+          <option value="comedy" data-testid="genre-option">Comédia</option>
           <option value="thriller" data-testid="genre-option">Suspense</option>
         </select>
       </label>
