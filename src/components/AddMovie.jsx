25247bd8ee9e57d77e55ff80849cf87399a3016b
Mayara <mayandronico@msn.com>
2021-09-13 17:39:13 -0300

    Corrige lint novamente

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,23 +1,22 @@
-// implement AddMovie component here
-import React from 'react';
-import PropTypes from 'prop-types';
+// // implement AddMovie component here
+// import React from 'react';
+// import PropTypes from 'prop-types';
 
-class AddMovie extends React.Component {
-  render() {
-    const onClick = this.props;
+// class AddMovie extends React.Component {
+// //   render() {
+// //     const onClick = this.props;
 
-      return (
-        <form data-testid="add-movie-form">
-          
-              <option data-testid="select-option" value="">Todos</option>
-              <option data-testid="select-option" value="action">Ação</option>
-              <option data-testid="select-option" value="comedy">Comédia</option>
-              <option data-testid="select-option" value="thriller">Suspense</option>
-     </form>
-    );
-  }
-}
-AddMovie.propTypes = {
-  onClick: PropTypes.func.isRequired,
-};
-export default AddMovie;
+// //     return (
+// //       <form data-testid="add-movie-form">
+// //         <option data-testid="select-option" value="">Todos</option>
+// //         <option data-testid="select-option" value="action">Ação</option>
+// //         <option data-testid="select-option" value="comedy">Comédia</option>
+// //         <option data-testid="select-option" value="thriller">Suspense</option>
+// //       </form>
+// //     );
+// //   }
+// // }
+// // AddMovie.propTypes = {
+// //   onClick: PropTypes.func.isRequired,
+// // };
+// export default AddMovie;
