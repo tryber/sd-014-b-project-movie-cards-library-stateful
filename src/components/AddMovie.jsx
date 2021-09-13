63e25dd285291fa28a46b1c1e5ec748708a5b165
larymanzo <lamosinha@hotmail.com>
2021-09-12 22:23:42 -0300

    Requisito 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,71 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [name]: value });
+  }
+
   render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
-      <form>
-        <label htmlFor="sub">
-          <input type="text" name="sub" />
+      <form data-testid="add-movie-form">
+        <label htmlFor="subtitle">
+          <input
+            type="text"
+            name="subtitle"
+            value={ subtitle }
+            onChange={ this.handleInputChange }
+          />
+        </label>
+        <label htmlFor="Título" data-testid="title-input-label">
+          <input
+            type="text"
+            name="Título"
+            value={ title }
+            onChange={ this.handleInputChange }
+          />
+        </label>
+        <label htmlFor="imagePath">
+          <input
+            type="image"
+            alt=""
+            name="imagePath"
+            value={ imagePath }
+            onChange={ this.handleInputChange }
+          />
+        </label>
+        <label htmlFor="subtitle">
+          <input
+            type="text"
+            name="subtitle"
+            value={ storyline }
+            onChange={ this.handleInputChange }
+          />
+        </label>
+        <label htmlFor="subtitle">
+          <input
+            type="text"
+            name="subtitle"
+            value={ rating }
+            onChange={ this.handleInputChange } />
+        </label>
+        <label htmlFor="subtitle">
+          <input type="text" name="subtitle" value={ genre } onChange={ this.handleInputChange } />
         </label>
       </form>
     );
