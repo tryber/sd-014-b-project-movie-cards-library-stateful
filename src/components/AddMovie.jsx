f1d7a8740a7c3f55de8e660c4af22b0497edaa60
GustavoDamm <gustavo.damm15@hotmail.com>
2021-09-13 00:37:01 -0300

    addmovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,5 +1,10 @@
-/* eslint-disable react/no-unused-state */
 import React from 'react';
+import SubtitleAdd from './SubtitleAdd';
+import TitleAdd from './TitleAdd';
+import ImagePathAdd from './ImagePathAdd';
+import StorylineAdd from './StorylineAdd';
+import RatingAdd from './RatingAdd';
+import GenreAdd from './GenreAdd';
 
 class AddMovie extends React.Component {
   constructor() {
@@ -16,19 +21,33 @@ class AddMovie extends React.Component {
     this.AddMovie = this.AddMovie.bind(this);
   }
 
-  handleChange({ target }) {
+  AddMovie({ target }) {
     const { name, value } = target;
 
     this.setState({ [name]: value });
   }
 
   render() {
+    const { state } = this;
     return (
       <section>
-        a
+        <form htmlFor="search" data-testid="add-movie-form">
+          <SubtitleAdd addMovie={ this.addMovie } value={ state.subtitle } />
+          <TitleAdd value={ state.title } addMovie={ this.addMovie } />
+          <ImagePathAdd value={ state.imagePath } addMovie={ this.addMovie } />
+          <StorylineAdd value={ state.storyline } addMovie={ this.addMovie } />
+          <RatingAdd value={ state.rating } addMovie={ this.addMovie } />
+          <GenreAdd value={ state.genre } addMovie={ this.addMovie } />
+          <button type="submit" data-testid="send-button">Adicionar Filme</button>
+        </form>
       </section>
     );
   }
 }
+
+RatingAdd.propTypes = {
+  value: PropTypes.string.isRequired,
+  addMovie: PropTypes.func.isRequired,
+};
 
 export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
@@ -7,15 +6,20 @@ import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
   render() {
+    const { movies } = this.props;
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={ this.props.movies } />
+        <MovieList movies={ movies } />
         <AddMovie />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.string.isRequired,
+};
 
 export default MovieLibrary;
