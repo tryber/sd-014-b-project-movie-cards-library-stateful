d77b12d8ec1a02b3bc49499b4f5938a8c701326f
larymanzo <lamosinha@hotmail.com>
2021-09-12 21:59:52 -0300

    Requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,15 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  render() {
+    return (
+      <form>
+        <label htmlFor="sub">
+          <input type="text" name="sub" />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
