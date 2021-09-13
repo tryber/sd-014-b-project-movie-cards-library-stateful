2bd7482dd80316fc108caf7d31812c11f9ce7209
vinicius-mns <vinicius_mns@hotmail.com>
2021-09-13 12:58:41 -0300

    requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,11 @@
-// implement MovieLibrary component here
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <div>requisito 15</div>
+    );
+  }
+}
+
+export default MovieLibrary;
