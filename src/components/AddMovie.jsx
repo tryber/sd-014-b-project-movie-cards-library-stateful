e8f962e42259347761ec65185435a3c0b558de8a
vinicius-mns <vinicius_mns@hotmail.com>
2021-09-12 21:51:24 -0300

    requisito numero 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,26 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    // this.adiociona = this.adiociona.bind(this)
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    return (
+      <div>nada</div>
+    );
+  }
+}
+
+export default AddMovie;
