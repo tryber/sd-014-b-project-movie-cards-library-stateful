61b2376740041f82501ff466dfc06e191939119e
= <luz.matheus11@gmail.com>
2021-09-14 19:33:18 -0300

    Req 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,23 @@
-// implement SearchBar component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>SearchBar</h1>
+      </div>
+    );
+  }
+}
+
+SearchBar.protoTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func,
+  selectedGenre: PropTypes.string,
+  onSelectedGenreChange: PropTypes.func,
+};
+
+export default SearchBar;
