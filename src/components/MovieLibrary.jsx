6b3bd11045b9d89ec6477a1b86fee0e792c0c7ee
Arturo Vaine <arturo.vaine@gmail.com>
2021-09-14 18:58:58 -0300

    Requisitos 13 e 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,29 +1,23 @@
 import React, { Component } from 'react';
+
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
 import AddMovie from './AddMovie';
 
-// import PropTypes from 'prop-types';
-// import { movies } from '../data';
+import data from '../data';
 
 class MovieLibrary extends Component {
-//  constructor(props) {
-//    super(props);
-//  }
 
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList />
+        <MovieList movies={ data } />
         <AddMovie />
       </div>
     );
   }
 }
-/*
-MovieLibrary.PropTypes = {
-};
-*/
+
 export default MovieLibrary;
