a5be85f444e68aedbfcd508be3fac674d00f5552
= <luz.matheus11@gmail.com>
2021-09-14 19:46:50 -0300

    Req 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 class SearchBar extends React.Component {
   render() {
     return (
-      <div>
-        <h1>SearchBar</h1>
-      </div>
+      <form data-testid="search-bar-form">
+        <input />
+      </form>
     );
   }
 }
