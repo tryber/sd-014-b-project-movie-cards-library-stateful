d63542550a5a794e77864a99dd08c5946b332ffb
GustavoDamm <gustavo.damm15@hotmail.com>
2021-09-14 15:34:36 -0300

    State

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,24 +1,72 @@
+/* eslint-disable react/no-unused-state */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
-/* import data from '../data'; */
 
 class MovieLibrary extends Component {
-  onClick = (state) => {
-    const { movies } = this.state;
+  constructor(props) {
+    super();
+
+    this.state = {
+      searchText: '',
+      boobkmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkChange = this.onBookmarkChange.bind(this);
+    this.onSelectedGenreChange = this.onBookmarkChange.bind(this);
+  }
+
+  onSearchTextChange({ target }) {
+    const { movies, searchText } = this.state;
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
-      movies: [...movies, state],
+      [name]: value,
+      movies: movies.filter((element) => (
+        element.title.includes(searchText)
+        || element.subtitle.includes(searchText)
+        || element.storyline.includes(searchText)
+      )),
     });
   }
 
+  onBookmarkChange({ target }) {
+    const { movies } = this.state;
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const bookmark = movies.filter((element) => element.bookmarked === true);
+    if (target.checked) {
+      this.setState({
+        [name]: value,
+        movies: bookmark,
+      });
+    }
+  }
+
+  onSelectedGenreChange({ target }) {
+    const { movies } = this.state;
+    const { name, value } = target;
+    const genre = movies.filter((element) => element.genre === value);
+    if (target.checked) {
+      this.setState({
+        [name]: value,
+        movies: genre,
+      });
+    }
+  }
+
   render() {
-    const { movies } = this.props;
+    const { searchText, boobkmarkedOnly, movies } = this.state;
     return (
       <div>
-        <h2> My awesome movie library </h2>
-        <SearchBar />
+        <SearchBar
+          searchText={ searchText }
+          boobkmarkedOnly={ boobkmarkedOnly }
+        />
         <MovieList movies={ movies } />
         <AddMovie />
       </div>
@@ -27,7 +75,9 @@ class MovieLibrary extends Component {
 }
 
 MovieLibrary.propTypes = {
-  movies: PropTypes.string.isRequired,
+  movies: PropTypes.arrayOf(
+    PropTypes.array,
+  ).isRequired,
 };
 
 export default MovieLibrary;
