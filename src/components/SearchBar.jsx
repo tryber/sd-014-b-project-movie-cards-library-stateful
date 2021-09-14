7a8c46a100760f184d6e732827c35177de06ab99
Thiago Gomez <thiagogomez2906@hotmail.com>
2021-09-14 10:54:00 -0300

    requisito 4 e 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -16,6 +16,19 @@ class SearchBar extends React.Component {
         <label data-testid="text-input-label" for="name">Inclui o texto:</label>
         <input type="text" value={ searchText } onChange={ onSearchTextChange }
         name="name" data-testid="text-input" ></input>
+
+        <label data-testid="checkbox-input-label" for="fav">Mostrar somente favoritos</label>
+        <input type="checkbox" checked={ bookmarkedOnly } onChange={ onBookmarkedChange }
+        name="fav" data-testid="checkbox-input" ></input>
+        
+        <label data-testid="select-input-label" for="genre">Filtrar por gênero</label>
+        <select value={ selectedGenre } onChange={ onSelectedGenreChange }
+        name="genre" data-testid="select-input">
+          <option value="" data-testid="select-option">Todos</option>
+          <option value="action" data-testid="select-option">Ação</option>
+          <option value="comedy" data-testid="select-option">Comédia</option>
+          <option value="thriller" data-testid="select-option">Suspense</option>
+        </select>
       </form>
     );
   }
