d782e04c46abef1640f07efd50bba02dec7cff5c
Everson Elias Gonçalves de Oliveira <eversoneliasg@gmail.com>
2021-09-13 22:03:42 -0300

    Componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,44 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Title from './Title';
+
+export class AddMovie extends Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  handleChange({ target }) {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const { onClick } = this.props;
+    const { subtitle, title, imagePath, storyLine, rating, genre } = this.state;
+
+    return (
+      <form data-testid="add-movie-form">
+        <Title value={ title } onChange={ this.handleChange } />
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onclick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
