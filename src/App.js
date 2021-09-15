a6bf11e322054bb98b60eafdfae8e642cd5c91e4
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-09-14 21:18:06 -0300

    Requesito 15.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import './App.css';
 import SearchBar from './components/SearchBar';
 import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <SearchBar />
       <AddMovie />
+      <MovieLibrary />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,23 @@
 // implement MovieLibrary component here
+import React, { Component } from 'react';
+/* import PropTypes from 'prop-types'; */
+
+class MovieLibrary extends Component {
+  /* constructor() {
+    super()
+
+  } */
+
+  render() {
+    /* const { movies } = this.props; */
+    return (
+      <p>hello</p>
+    );
+  }
+}
+
+/* MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+}; */
+
+export default MovieLibrary;
