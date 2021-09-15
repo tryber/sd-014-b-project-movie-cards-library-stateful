68f777654dbf39fcf6929c42389b3b167fd86982
Daniel <daniel.acoelho@outlook.com>
2021-09-15 17:21:49 -0300

    ta dificil passar no eslint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -17,28 +17,28 @@ class SearchBar extends React.Component {
               onChange={ onSearchTextChange }
             />
           </label>
-            <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
-              Mostrar somente favoritos
-              <input
-                data-testid="checkbox-input"
-                type="checkbox"
-                checked={ bookmarkedOnly }
-                onChange={ onBookmarkedChange }
-              />
-            </label>
-            <label htmlFor="select-input" data-testid="select-input-label">
-              Filtrar por gênero
-              <select
-                data-testid="select-input"
-                value={ selectedGenre }
-                onChange={ onSelectedGenreChange }
-              >
-                <option data-testid="select-option" value="">Todos</option>
-                <option data-testid="select-option" value="action">Ação</option>
-                <option data-testid="select-option" value="comedy">Comédia</option>
-                <option data-testid="select-option" value="thriller">Suspense</option>
-              </select>
-            </label>
+          <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
+            Mostrar somente favoritos
+            <input
+              data-testid="checkbox-input"
+              type="checkbox"
+              checked={ bookmarkedOnly }
+              onChange={ onBookmarkedChange }
+            />
+          </label>
+          <label htmlFor="select-input" data-testid="select-input-label">
+            Filtrar por gênero
+            <select
+              data-testid="select-input"
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+            >
+              <option data-testid="select-option" value="">Todos</option>
+              <option data-testid="select-option" value="action">Ação</option>
+              <option data-testid="select-option" value="comedy">Comédia</option>
+              <option data-testid="select-option" value="thriller">Suspense</option>
+            </select>
+          </label>
         </section>
       </form>
     );
