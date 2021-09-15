57079e902c29d4a0a8d102aa7b4c9a171519226c
Mayara <mayandronico@msn.com>
2021-09-15 00:53:59 -0300

    Corrige lint

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,29 +1,29 @@
-// implement MovieLibrary component here
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import SearchBar from './SearchBar';
-import AddMovie from './AddMovie';
+// // implement MovieLibrary component here
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import SearchBar from './SearchBar';
+// import AddMovie from './AddMovie';
 
-class MovieLibrary extends Component {
-    constructor() {
-        super();
-        this.state = {
-          searchText: false ,
-          bookmarkedOnly: '',
-          selectedGenre: '',
-          movies: movies,
-        };
+// class MovieLibrary extends Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//           searchText: false ,
+//           bookmarkedOnly: '',
+//           selectedGenre: '',
+//           movies: movies,
+//         };
 
-  render() {
-    const { movies } = this.props;
-    return (
-      <section></section>
-    );
-  }
-}
+// //   render() {
+// //     const { movies } = this.props;
+// //     return (
+// //       <section></section>
+// //     );
+// //   }
+// // }
 
-MovieLibrary.propTypes = {
-  movies: PropTypes.shape({}).isRequired,
-};
+// // MovieLibrary.propTypes = {
+// //   movies: PropTypes.shape({}).isRequired,
+// // };
 
-export default MovieLibrary;
+// // export default MovieLibrary;
