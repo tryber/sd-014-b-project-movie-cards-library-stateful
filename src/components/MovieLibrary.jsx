f29da972ec209cfceaf2d6a4a76e44fb9a7aeec6
Arturo Vaine <arturo.vaine@gmail.com>
2021-09-15 17:39:34 -0300

    MovieLibrary com setstate movies

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,19 +2,35 @@ import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
 import AddMovie from './AddMovie';
-import data from '../data';
 
 class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      movies,
+    };
+  }
+
   render() {
+    const { movies } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
+
         <SearchBar />
-        <MovieList movies={ data } />
+
+        <MovieList movies={ movies } />
+
         <AddMovie />
+
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieLibrary;
