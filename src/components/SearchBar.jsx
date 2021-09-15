aed97e9e4eeb9c6995969c121109de44e22f6e2b
viniciussantana <viniciusantana@yahoo.com.br>
2021-09-15 16:18:31 -0300

    [Requisito 2]

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,7 +12,9 @@ class SearchBar extends React.Component {
   render() {
     const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
-      <div />
+      <form data-testid="search-bar-form">
+
+      </form>
     );
   }
 }
