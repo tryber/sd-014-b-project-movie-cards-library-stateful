d78b9596b8451f9c42d32db2ddb526e1f5ba6efc
Jean Marcel <jeanvanzella91@gmail.com>
2021-09-14 23:44:22 -0300

    req 10 e 11

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -13,11 +13,48 @@ class AddMovie extends React.Component {
     };
   }
 
+  changeTitle = (event) => {
+    this.setState({
+      title: event.target.value,
+    });
+  }
+
+  changeImage = (event) => {
+    this.setState({
+      imagePath: event.target.value,
+    });
+  }
+
   render() {
     const { onClick } = this.props;
+    const {
+      subtitle,
+      title,
+      imagePath,
+      storyline,
+      rating,
+      genre,
+    } = this.state;
     return (
       <form data-testid="add-movie-form">
-        a
+        <label htmlFor="Título">
+          Título
+          <input
+            type="text"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.changeTitle }
+          />
+        </label>
+        <label htmlFor="Imagem" data-testid="image-input-label">
+          Imagem
+          <input
+            type="text"
+            value={ imagePath }
+            data-testid="image-input"
+            onChange={ this.changeImage }
+          />
+        </label>
       </form>
     );
   }
