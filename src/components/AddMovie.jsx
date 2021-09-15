9234083e8bdec4f75913a4d538baee2e66a1063a
Jean Marcel <jeanvanzella91@gmail.com>
2021-09-15 00:12:25 -0300

    req 12 e 13

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -19,9 +19,33 @@ class AddMovie extends React.Component {
     });
   }
 
+  changeSubtitle = (event) => {
+    this.setState({
+      subtitle: event.target.value,
+    });
+  }
+
   changeImage = (event) => {
     this.setState({
       imagePath: event.target.value,
+    });
+  }
+
+  changeStoryLine = (event) => {
+    this.setState({
+      storyline: event.target.value,
+    });
+  }
+
+  changeRating = (event) => {
+    this.setState({
+      rating: event.target.value,
+    });
+  }
+
+  changeGenre = (event) => {
+    this.setState({
+      genre: event.target.value,
     });
   }
 
@@ -46,6 +70,15 @@ class AddMovie extends React.Component {
             onChange={ this.changeTitle }
           />
         </label>
+        <label htmlFor="Subtítulo" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            data-testid="subtitle-input"
+            value={ subtitle }
+            onChange={ this.changeSubtitle }
+          />
+        </label>
         <label htmlFor="Imagem" data-testid="image-input-label">
           Imagem
           <input
@@ -55,12 +88,35 @@ class AddMovie extends React.Component {
             onChange={ this.changeImage }
           />
         </label>
+        <label htmlFor="Sinopse" data-testid="storyline-input-label">
+          Sinopse
+          <textarea
+            value={ storyline }
+            cols="30"
+            rows="10"
+            data-testid="storyline-input"
+            onChange={ this.changeStoryLine }
+          >
+          </textarea>
+        </label>
+        <label htmlFor="Avaliação" data-testid="rating-input-label">
+          Avaliação
+          <input type="number" value={ rating } onChange={ this.changeRating } />
+        </label>
+        <label htmlFor="Gênero" data-testid="genre-input-label">
+          Gênero
+          <select value={ genre } data-testid="genre-input" onChange={ this.changeGenre }>
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
 }
 
-AddMovie.propType = {
+AddMovie.propTypes = {
   onClick: PropTypes.func.isRequired,
 };
 
