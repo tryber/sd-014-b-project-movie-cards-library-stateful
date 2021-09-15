12aa2f77c7d556d2c542aee7a570433fa2b83c7c
mfelipperd <felipperabelodurans@gmail.com>
2021-09-15 18:27:39 -0300

    requisito 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,12 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div>
+        <input type="text" />
+      </div>
+    );
+  }
+}
+export default SearchBar;
