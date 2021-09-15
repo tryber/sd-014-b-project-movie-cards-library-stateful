7de1a2e7d6bedf93d46cc79862b89db22bca2ac7
Daniel <daniel.acoelho@outlook.com>
2021-09-15 15:45:38 -0300

    ta dificil passar no eslint

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
-
-// import MovieList from './MovieList';
+import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
+import MovieList from './MovieList';
 
 class MovieLibrary extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
   render() {
+    const movies = this.props;
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        {/* <MovieList movies={ this.props.movies } /> */}
+        <MovieList movies={ movies } />
         <AddMovie />
       </div>
     );
   }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
 }
 
 export default MovieLibrary;
