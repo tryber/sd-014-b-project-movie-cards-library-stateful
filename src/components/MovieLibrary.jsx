d36bf37b5f7bd442d0b9e8e4a0c80111ab380c1b
Mayara <mayandronico@msn.com>
2021-09-15 15:16:50 -0300

    Requisito 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,29 +1,27 @@
-// // implement MovieLibrary component here
-// import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
-// import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+// implement MovieLibrary component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
 
-// class MovieLibrary extends Component {
-//     constructor() {
-//         super();
-//         this.state = {
-//           searchText: false ,
-//           bookmarkedOnly: '',
-//           selectedGenre: '',
-//           movies: movies,
-//         };
+class MovieLibrary extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section>
+        <SearchBar />
+        <AddMovie movies={ movies } />
+        <MovieList movies={ movies } />
+      </section>
+    );
+  }
+}
 
-// //   render() {
-// //     const { movies } = this.props;
-// //     return (
-// //       <section></section>
-// //     );
-// //   }
-// // }
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
 
-// // MovieLibrary.propTypes = {
-// //   movies: PropTypes.shape({}).isRequired,
-// // };
-
-// // export default MovieLibrary;
+export default MovieLibrary;
