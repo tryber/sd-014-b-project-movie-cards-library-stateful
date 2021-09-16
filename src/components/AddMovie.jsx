fd8f9a143e7929256a35c63f74595c094107c3a1
Alexandre Fortes <fortes04alexandre@gmail.com>
2021-09-16 06:23:26 -0300

    AddMovie parcial

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,75 @@
 // implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import InputMovie from './InputMovie';
+import InputGenre from './InputGenre';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+    handleChange = ({ target }) => {
+      const { name, value } = target;
+      this.setState({
+        [name]: value,
+      });
+    }
+
+      handleClick = (event) => {
+        event.preventDefault();
+        const { onClick } = this.props;
+        onClick(this.state);
+        this.setState({
+          subtitle: '',
+          title: '',
+          imagePath: '',
+          storyline: '',
+          rating: 0,
+          genre: 'action',
+        });
+      }
+
+      render() {
+        const { title, subtitle, storyline, rating, genre, imagePath } = this.state;
+        return (
+          <section>
+            <form data-testid="add-movie-form">
+              <InputMovie
+                title={ title }
+                subtitle={ subtitle }
+                storyline={ storyline }
+                imagePath={ imagePath }
+                onChange={ this.handleChange }
+              />
+              <InputGenre
+                rating={ rating }
+                genre={ genre }
+                onChange={ this.handleChange }
+              />
+              <button
+                type="submit"
+                data-testid="send-button"
+                onClick={ this.handleClick }
+              >
+                Adicionar filme
+              </button>
+            </form>
+          </section>
+        );
+      }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
