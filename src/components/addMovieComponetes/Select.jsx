0779773082630b9f508f81c47460060b0566b626
= <luz.matheus11@gmail.com>
2021-09-16 10:56:16 -0300

    Req 13

--- src/components/addMovieComponetes/Select.jsx
+++ src/components/addMovieComponetes/Select.jsx
@@ -5,18 +5,18 @@ class Select extends React.Component {
   render() {
     const { value, onChange } = this.props;
     return (
-      <label data-testid="genre-input-label" htmlFor="genre-input-number">
+      <label data-testid="genre-input-label" htmlFor="genre-input-option">
         Gênero
         <select
           data-testid="genre-input"
-          id="genre-input-number"
+          id="genre-input-option"
           name="genre"
           value={ value }
           onChange={ onChange }
         >
           <option data-testid="genre-option" value="action">Ação</option>
           <option data-testid="genre-option" value="comedy">Comédia</option>
-          <option data-testid="genre-option" value="thriller">Supense</option>
+          <option data-testid="genre-option" value="thriller">Suspense</option>
         </select>
       </label>
     );
