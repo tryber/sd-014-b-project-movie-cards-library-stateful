d47b41b594c22c1f37079b8e0459f2a7bf3ca70d
Juan Formoso <trajano.juan@gmail.com>
2021-09-15 23:10:12 -0400

    Add Search Bar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+/* import React, { Component } from 'react';
 
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
@@ -14,7 +14,7 @@ class MovieLibrary extends Component {
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={this.props.movies} />
+        <MovieList movies={ this.props.movies } />
         <AddMovie />
       </div>
     );
@@ -22,4 +22,4 @@ class MovieLibrary extends Component {
 }
 
 export default MovieLibrary;
-
+ */
