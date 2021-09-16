e32b961e5fc0e651c586b3f41f71a45ca28d432e
mfelipperd <felipperabelodurans@gmail.com>
2021-09-16 17:45:35 -0300

    requisitos 15 a 17 e primeira funçao generica

---
+++ src/components/MovieLibrary.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.handleGeneric = this.handleGeneric.bind(this);
+  }
+
+  handleGeneric({ target }) {
+    const { value } = target;
+    this.setState({
+      [Name]: value,
+    });
+  }
+
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.string,
+}.isRequired;
+export default MovieLibrary;
