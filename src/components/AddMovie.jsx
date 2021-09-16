cb9b28ce58999f86a26fd95c65b8ed852bc6eb37
viniciussantana <viniciusantana@yahoo.com.br>
2021-09-15 21:30:16 -0300

    [Requisito 13][addMovie][lint]

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -64,7 +64,12 @@ class addMovie extends React.Component {
         <RatingInput value={ rating } handleChange={ handleChange } />
         <label htmlFor="genre-input" data-testid="genre-input-label">
           Gênero
-          <select value={ genre } name="genre" data-testid="genre-input" onChange={ handleChange }>
+          <select
+            value={ genre }
+            name="genre"
+            data-testid="genre-input"
+            onChange={ handleChange }
+          >
             <option data-testid="genre-option" value="action">Ação</option>
             <option data-testid="genre-option" value="comedy">Comédia</option>
             <option data-testid="genre-option" value="thriller">Suspense</option>
