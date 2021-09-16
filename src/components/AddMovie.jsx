2f21f1325659c338ce244d986318e6342f4a2303
Daniel <daniel.acoelho@outlook.com>
2021-09-16 12:43:28 -0300

    correcoes de linte requisitos ate o 14 4a tentativa mais lint

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -5,66 +5,65 @@ import Title from './Title';
 import Subtitle from './Subtitle';
 import Story from './Story';
 import Genre from './Genre';
-import InputRating from './InputRating.jsx';
+import InputRating from './InputRating';
 
 class AddMovie extends React.Component {
-constructor(props) {
-super(props);
-this.handleClick = this.handleClick.bind(this);
-this.handleChange = this.handleChange.bind(this);
-this.state = {
-subtitle: '',
-title: '',
-imagePath: '',
-storyline: '',
-rating: 0,
-genre: 'action',
-};
-}
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
 
-handleClick() {
-const { onClick } = this.props;
-onClick(this.state);
-this.setState({
-subtitle: '',
-title: '',
-imagePath: '',
-storyline: '',
-rating: 0,
-genre: 'action',
-});
-}
+  handleClick() {
+    const { onClick } = this.props;
+    onClick(this.state);
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
 
-handleChange({ target }) {
-const { name, value } = target;
-this.setState({
-[name]: value,
-});
-}
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
 
-render() {
-const { imagePath, title, subtitle, storyline, rating, genre } = this.state;
-return (
-<section>
-  <form data-testid="add-movie-form">
-    <Image imagePath={ imagePath } handleChange={ this.handleChange } />
-    <Title title={ title } handleChange={ this.handleChange } />
-    <Subtitle subtitle={ subtitle } handleChange={ this.handleChange } />
-    <Story storyline={ storyline } handleChange={ this.handleChange } />
-    <InputRating rating={ rating } handleChange={ this.handleChange } />
-    <Genre genre={ genre } handleChange={ this.handleChange } />
-    <button type="submit" data-testid="send-button" onClick={ this.handleClick }>
-      Adicionar filme
-    </button>
-  </form>
-</section>
-);
-}
+  render() {
+    const { imagePath, title, subtitle, storyline, rating, genre } = this.state;
+    return (
+      <section>
+        <form data-testid="add-movie-form">
+          <Image imagePath={ imagePath } handleChange={ this.handleChange } />
+          <Title title={ title } handleChange={ this.handleChange } />
+          <Subtitle subtitle={ subtitle } handleChange={ this.handleChange } />
+          <Story storyline={ storyline } handleChange={ this.handleChange } />
+          <InputRating rating={ rating } handleChange={ this.handleChange } />
+          <Genre genre={ genre } handleChange={ this.handleChange } />
+          <button type="submit" data-testid="send-button" onClick={ this.handleClick }>
+            Adicionar filme
+          </button>
+        </form>
+      </section>
+    );
+  }
 }
 
 AddMovie.propTypes = {
-onClick: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default AddMovie;
-
