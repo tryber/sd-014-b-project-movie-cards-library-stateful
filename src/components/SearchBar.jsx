2147c7c5a66180d43ebd5f4a615466f818fb560d
Bruno Faria <contatobfimportados@gmail.com>
2021-09-18 17:48:51 -0300

    Req 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -16,6 +16,15 @@ class SearchBar extends React.Component {
             data-testid="text-input"
           />
         </label>
+        <label data-testid="checkbox-input-label" htmlFor="favorites">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+        </label>
       </form>
     );
   }
