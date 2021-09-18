e22b6cdbc52cfe71d1d4b548c3e73f61a9482a3f
Bruno Faria <contatobfimportados@gmail.com>
2021-09-18 17:47:49 -0300

    Req 1 e 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-class SearchBar extends Component {
+class SearchBar extends React.Component {
   render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange } = this.props;
+
     return (
-      <div>
-        <h1> Algo </h1>
-      </div>
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="input-text">
+        </label>
+      </form>
     );
   }
 }
