c19f7f3bd09849c5c39c622423ef4323d9cbb858
Bruno Faria <contatobfimportados@gmail.com>
2021-09-18 17:57:46 -0300

    Req 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -25,6 +25,19 @@ class SearchBar extends React.Component {
             data-testid="checkbox-input"
           />
         </label>
+        <label data-testid="select-input-label" htmlFor="genre">
+          Filtrar por gênero:
+          <select
+            data-testid="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
