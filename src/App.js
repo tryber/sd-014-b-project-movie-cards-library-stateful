5bb32687669371e9bf107473c1cf9c9659ca4970
Bruno Faria <contatobfimportados@gmail.com>
2021-09-18 18:35:56 -0300

    Req 6 a 14

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import SearchBar from './components/SearchBar';
+import AddMovie from './components/AddMovie';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <SearchBar />
+        <AddMovie />
       </div>
     );
   }
