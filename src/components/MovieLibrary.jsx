cab4185a652bd3f97fa79ffe57c4834c75d502cb
Bruno Faria <contatobfimportados@gmail.com>
2021-09-18 17:29:07 -0300

    segundo comit

---
+++ src/components/MovieLibrary.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,13 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Algo </h1>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
