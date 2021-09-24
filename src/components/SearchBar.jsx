491dcc4240b9da34a66ab5295ee504312037f2fb
IgorFellows <igor.fpp96@gmail.com>
2021-09-23 22:27:29 -0300

    Metade o 1 (falta corrigir o lint)

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,22 +1,50 @@
-import React from  'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
-    render () {
-        const {
-        seachText,
-        onSearchTextChange,
-        bookmarkedOnly,
-        onBookmarkedChange,
-        selectedGenre,
-        onSelectedGenreChange,
+  render() {
+    const {
+      seachText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
     } = this.props;
 
-        return (
-            <form data-testid="search-bar-form">
-            </form>
-        )
-    }
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="searchText" data-testid="text-input-label">
+          inclui o texto
+          <input
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
+        <label htmlFor="checkbox" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+        </label>
+
+      </form>
+    );
+  }
 }
 
-    export default SearchBar;
+SearchBar.propTypes = PropTypes.shape({
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func,
+  selectedGenre: PropTypes.string,
+  onSelectedGenreChange: PropTypes.func,
+}).isRequired;
+
+export default SearchBar;
