3a092b49fb41b1b942c5cee19881d5ce20bdd25a
Eduardomak <eduardomak92@gmail.com>
2021-09-26 12:56:31 -0300

    requisitos 9, 10, 11, 12

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,8 +4,26 @@ class AddMovie extends React.Component {
   render() {
     return (
       <form data-testid="add-movie-form">
-        <input type="text" data-testid="title-input"/>
-        <label htmlFor="title-input-label" data-testid="title-input-label">Título</label>
+        <label htmlFor="title-input-label" data-testid="title-input-label">
+          Título
+          <input type="text" data-testid="title-input" />
+        </label>
+        <label htmlFor="subtitle-input-label" data-testid="subtitle-input-label">
+          Subtítulo
+          <input type="text" data-testid="subtitle-input" />
+        </label>
+        <label htmlFor="image-input-label" data-testid="image-input-label">
+          Imagem
+          <input type="text" data-testid="image-input" />
+        </label>
+        <label htmlFor="storyline-input-label" data-testid="storyline-input-label">
+          Sinopse
+          <textarea name="storyline-input" id="storyline-input" cols="30" rows="10" data-testid="storyline-input" />
+        </label>
+        <label htmlFor="rating-input-label" data-testid="rating-input-label">
+          Avaliação
+          <input type="number" data-testid="rating-input" defaultValue={ 0 } />
+        </label>
       </form>
     );
   }
