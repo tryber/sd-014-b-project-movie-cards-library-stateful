9323ea250450f4e17cd40af9916d1290a45f6a8c
Eduardomak <eduardomak92@gmail.com>
2021-09-26 13:14:34 -0300

    requisito 13

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -24,6 +24,14 @@ class AddMovie extends React.Component {
           Avaliação
           <input type="number" data-testid="rating-input" defaultValue={ 0 } />
         </label>
+        <label htmlFor="genre-input-label" data-testid="genre-input-label">
+          Gênero
+          <select name="" id="" data-testid="genre-input">
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
