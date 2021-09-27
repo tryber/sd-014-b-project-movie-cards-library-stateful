4a4f2ccdf6f580b0cbc2b26ff2214ea5961667b6
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-09-27 15:22:06 -0300

    AddButton component

--- src/components/ButtonAddMovie.jsx
+++ src/components/ButtonAddMovie.jsx
@@ -5,13 +5,13 @@ class ButtonAddMovie extends React.Component {
   render() {
     const { handleClick } = this.props;
     return (
-      <button type="submit" data-testid="send-button" onClick={ handleClick }>
+      <button type="button" data-testid="send-button" onClick={ handleClick }>
         Adicionar filme
       </button>
     );
   }
 }
 
-ButtonAddMovie.propTypes = PropTypes.oneOf({ onClick: PropTypes.func }).isRequired;
+ButtonAddMovie.propTypes = { handleClick: PropTypes.func.isRequired };
 
 export default ButtonAddMovie;
