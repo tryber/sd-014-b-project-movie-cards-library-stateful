4afe5e360e3e0c4f76b7471753d5d46701f92606
IgorFellows <igor.fpp96@gmail.com>
2021-09-29 03:22:56 -0300

    Fim

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -25,7 +25,7 @@ MovieCard.propTypes = {
     title: PropTypes.string,
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
-    rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    rating: propTypes.number,
     imagePath: PropTypes.string,
   }).isRequired,
 };
