71f79bacbef2f2c117b7bd73cff9fb078b2c9ac0
Eduardomak <eduardomak92@gmail.com>
2021-09-28 22:40:05 -0300

    requisito 19

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -6,21 +6,30 @@ import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  /*  constructor(props) {
+  constructor(props) {
     super(props);
+    const { movies } = this.props;
     this.state = {
+      movies,
+    };
+    this.addNewMovie = this.addNewMovie.bind(this);
+  }
 
-    }
-  } */
+  addNewMovie(addMovie) {
+    const { movies } = this.state;
+    this.setState({
+      movies: [...movies, addMovie],
+    });
+  }
 
   render() {
-    const { movies } = this.props;
+    const { movies } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
         <MovieList movies={ movies } />
-        <AddMovie />
+        <AddMovie onClick={ this.addNewMovie } />
       </div>
     );
   }
