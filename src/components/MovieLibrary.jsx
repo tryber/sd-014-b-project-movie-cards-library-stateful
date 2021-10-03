e20a0f55fde87e810c6bac270efe568ea25404a3
Victor Martins <victor.martins.duarte@gmail.com>
2021-10-03 19:23:33 -0300

    Requirement 1

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+    };
+  }
+
+
+
+  render() {
+    const { searchText } = this.state;
+    return (
+      <SearchBar searchText={ searchText } />
+    );
+  }
+}
+
+export default MovieLibrary;
