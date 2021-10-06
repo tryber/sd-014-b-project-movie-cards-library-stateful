e397a9cd08c81ac6b100bb60ac32ca12d740892c
Antonio Martins <anmmartyns@gmail.com>
2021-10-06 01:52:32 -0300

    Requisitos15 e 16

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
+import data from './data';
+import MovieLibrary from './components/MovieLibrary';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar />
-      <AddMovie />
+      <MovieLibrary movies={ data } />
     </div>
   );
 }
