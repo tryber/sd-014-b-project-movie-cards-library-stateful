a2332308affd7ac3fcb64bed720cd1be34acd774
LuctReis <luctreis@gmail.com>
2021-10-06 12:22:44 -0300

    Req 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -31,6 +31,21 @@ class SearchBar extends Component {
               onChange={ onBookmarkedChange }
             />
           </label>
+          <label data-testid="select-input-label" htmlFor="genre">
+            Filtrar por gênero
+            <select
+              data-testid="select-input"
+              name="selectedGenre"
+              id="genre"
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+            >
+              <option data-testid="select-option" value="">Todos</option>
+              <option data-testid="select-option" value="action">Ação</option>
+              <option data-testid="select-option" value="comedy">Comédia</option>
+              <option data-testid="select-option" value="thriller">Suspense</option>
+            </select>
+          </label>
         </form>
       </main>
     );
