f3d8110854ac80b38a54f418cd661a7bc88836c9
Theo Lima <theo.suporte@hotmail.com>
2021-10-05 23:01:29 -0300

    Começa implementaçao de AddMovies

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,38 @@
-// implement AddMovie component here
+import React from 'react';
+
+import Title from './inputs/Title';
+import Subtitle from './inputs/Subtitle';
+import ImagePath from './inputs/ImagePath';
+import Storyline from './inputs/StoryLine';
+import RatingInput from './inputs/RatingInput';
+import Genre from './inputs/Genre';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+        <Title title={ title } />
+        <Subtitle subtitle={ subtitle } />
+        <ImagePath imagePath={ imagePath } />
+        <Storyline storyline={ storyline } />
+        <RatingInput rating={ rating } />
+        <Genre genre={ genre } />
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
